mimblewimble/grin-wallet

Read version info from server, react accordingly (#154)
* read and parse version, bump hf

* rustfmt

* add foreign api middleware check

* rustfmt

* add middleware checks

* rustfmt

* add check for incoming pre-hf slates

* api tests

libwallet/src/internal/tx.rs
@@ -16,6 +16,8 @@
 
 use uuid::Uuid;
 
+use crate::grin_core::consensus::valid_header_version;
+use crate::grin_core::core::HeaderVersion;
 use crate::grin_keychain::{Identifier, Keychain};
 use crate::grin_util::Mutex;
 use crate::internal::{selection, updater};
@@ -53,6 +55,10 @@ where
 	}
 	slate.amount = amount;
 	slate.height = current_height;
+
+	if valid_header_version(current_height, HeaderVersion(1)) {
+		slate.version_info.block_header_version = 1;
+	}
 
 	// Set the lock_height explicitly to 0 here.
 	// This will generate a Plain kernel (rather than a HeightLocked kernel).

libwallet/src/lib.rs
@@ -52,13 +52,16 @@ mod types;
 
 pub use crate::error::{Error, ErrorKind};
 pub use crate::slate::{ParticipantData, ParticipantMessageData, Slate};
-pub use crate::slate_versions::{SlateVersion, VersionedSlate};
+pub use crate::slate_versions::{
+	SlateVersion, VersionedSlate, CURRENT_SLATE_VERSION, GRIN_BLOCK_HEADER_VERSION,
+};
 pub use api_impl::types::{
 	BlockFees, CbData, InitTxArgs, InitTxSendArgs, IssueInvoiceTxArgs, NodeHeightResult,
 	OutputCommitMapping, SendTXArgs, VersionInfo,
 };
 pub use internal::restore::{check_repair, restore};
 pub use types::{
-	AcctPathMapping, BlockIdentifier, Context, NodeClient, OutputData, OutputStatus, TxLogEntry,
-	TxLogEntryType, TxWrapper, WalletBackend, WalletInfo, WalletInst, WalletOutputBatch,
+	AcctPathMapping, BlockIdentifier, Context, NodeClient, NodeVersionInfo, OutputData,
+	OutputStatus, TxLogEntry, TxLogEntryType, TxWrapper, WalletBackend, WalletInfo, WalletInst,
+	WalletOutputBatch,
 };

src/bin/cmd/wallet.rs
@@ -18,9 +18,12 @@ use clap::ArgMatches;
 use grin_wallet_config::WalletConfig;
 use grin_wallet_impls::{HTTPNodeClient, WalletSeed, SEED_FILE};
 use grin_wallet_libwallet::NodeClient;
+use semver::Version;
 use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
+
+const MIN_COMPAT_NODE_VERSION: &str = "2.0.0-beta.1";
 
 pub fn _init_wallet_seed(wallet_config: WalletConfig, password: &str) {
 	if let Err(_) = WalletSeed::from_file(&wallet_config, password) {
@@ -44,32 +47,29 @@ pub fn wallet_command(wallet_args: &ArgMatches<'_>, config: GlobalWalletConfig) 
 	// just get defaults from the global config
 	let wallet_config = config.members.unwrap().wallet;
 
-	// TODO: Very temporary code to obsolete grin wallet for the first hard fork
-	// All tx operations call get_chain_height as a first order of business,
-	// so this is the most non-intrusive place to put this
+	// Check the node version info, and exit with report if we're not compatible
 	let mut node_client = HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
 	let global_wallet_args = wallet_args::parse_global_args(&wallet_config, &wallet_args)
 		.expect("Can't read configuration file");
 	node_client.set_node_api_secret(global_wallet_args.node_api_secret.clone());
 
-	match node_client.clone().chain_height() {
-		Ok(h) => {
-			if h >= 262080 {
-				let err_str = "This version of grin-wallet is obsolete as of block 252080. Please download v2.0.0 from https://github.com/mimblewimble/grin-wallet/releases";
-				error!("{}", err_str);
-				println!();
-				println!("***************");
-				println!("{}", err_str);
-				println!("***************");
-				println!("(You can still view your balances by disconnecting from the grin node, however you will not be able to transact until you upgrade)");
-				println!();
-				return 1;
-			}
+	// This will also cache the node version info for calls to foreign API check middleware
+	if let Some(v) = node_client.clone().get_version_info() {
+		// Isn't going to happen just yet (as of 2.0.0) but keep this here for
+		// the future. the nodeclient's get_version_info will return 1.0 if
+		// it gets a 404 for the version function
+		if Version::parse(&v.node_version) < Version::parse(MIN_COMPAT_NODE_VERSION) {
+			let version = if v.node_version == "1.0.0" {
+				"1.0.x series"
+			} else {
+				&v.node_version
+			};
+			println!("Specified Grin Node (version {}) is outdated and incompatible with this wallet version", version);
+			println!("Please update the node or use a different one");
+			return 1;
 		}
-		// just continue if can't connect to node, as user won't be able to do
-		// anything meaninful anyhow
-		Err(_) => {}
 	}
+	// ... if node isn't available, allow offline functions
 
 	let res = wallet_args::wallet_command(wallet_args, wallet_config, node_client);
 

keplernetwork/kepler-wallet

libwallet/src/internal/tx.rs
@@ -17,6 +17,8 @@
 use uuid::Uuid;
 
 use crate::internal::{selection, updater};
+use crate::kepler_core::consensus::valid_header_version;
+use crate::kepler_core::core::HeaderVersion;
 use crate::kepler_keychain::{Identifier, Keychain};
 use crate::kepler_util::Mutex;
 use crate::slate::Slate;
@@ -53,6 +55,10 @@ where
 	}
 	slate.amount = amount;
 	slate.height = current_height;
+
+	if valid_header_version(current_height, HeaderVersion(1)) {
+		slate.version_info.block_header_version = 1;
+	}
 
 	// Set the lock_height explicitly to 0 here.
 	// This will generate a Plain kernel (rather than a HeightLocked kernel).

libwallet/src/lib.rs
@@ -52,13 +52,16 @@ mod types;
 
 pub use crate::error::{Error, ErrorKind};
 pub use crate::slate::{ParticipantData, ParticipantMessageData, Slate};
-pub use crate::slate_versions::{SlateVersion, VersionedSlate};
+pub use crate::slate_versions::{
+	SlateVersion, VersionedSlate, CURRENT_SLATE_VERSION, KEPLER_BLOCK_HEADER_VERSION,
+};
 pub use api_impl::types::{
 	BlockFees, CbData, InitTxArgs, InitTxSendArgs, IssueInvoiceTxArgs, NodeHeightResult,
 	OutputCommitMapping, SendTXArgs, VersionInfo,
 };
 pub use internal::restore::{check_repair, restore};
 pub use types::{
-	AcctPathMapping, BlockIdentifier, Context, NodeClient, OutputData, OutputStatus, TxLogEntry,
-	TxLogEntryType, TxWrapper, WalletBackend, WalletInfo, WalletInst, WalletOutputBatch,
+	AcctPathMapping, BlockIdentifier, Context, NodeClient, NodeVersionInfo, OutputData,
+	OutputStatus, TxLogEntry, TxLogEntryType, TxWrapper, WalletBackend, WalletInfo, WalletInst,
+	WalletOutputBatch,
 };

src/bin/cmd/wallet.rs
@@ -18,9 +18,12 @@ use clap::ArgMatches;
 use kepler_wallet_config::WalletConfig;
 use kepler_wallet_impls::{HTTPNodeClient, WalletSeed, SEED_FILE};
 use kepler_wallet_libwallet::NodeClient;
+use semver::Version;
 use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
+
+const MIN_COMPAT_NODE_VERSION: &str = "2.0.0-beta.1";
 
 pub fn _init_wallet_seed(wallet_config: WalletConfig, password: &str) {
 	if let Err(_) = WalletSeed::from_file(&wallet_config, password) {
@@ -43,32 +46,30 @@ pub fn seed_exists(wallet_config: WalletConfig) -> bool {
 pub fn wallet_command(wallet_args: &ArgMatches<'_>, config: GlobalWalletConfig) -> i32 {
 	// just get defaults from the global config
 	let wallet_config = config.members.unwrap().wallet;
-	// TODO: Very temporary code to obsolete kepler wallet for the first hard fork
-	// All tx operations call get_chain_height as a first order of business,
-	// so this is the most non-intrusive place to put this
+
+	// Check the node version info, and exit with report if we're not compatible
 	let mut node_client = HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
 	let global_wallet_args = wallet_args::parse_global_args(&wallet_config, &wallet_args)
 		.expect("Can't read configuration file");
 	node_client.set_node_api_secret(global_wallet_args.node_api_secret.clone());
 
-	match node_client.clone().chain_height() {
-		Ok(h) => {
-			if h >= 262080 {
-				let err_str = "This version of kepler-wallet is obsolete as of block 262080. Please download v2.0.0 from https://github.com/keplernetwork/kepler-wallet/releases";
-				error!("{}", err_str);
-				println!();
-				println!("***************");
-				println!("{}", err_str);
-				println!("***************");
-				println!("(You can still view your balances by disconnecting from the grin node, however you will not be able to transact until you upgrade)");
-				println!();
-				return 1;
-			}
+	// This will also cache the node version info for calls to foreign API check middleware
+	if let Some(v) = node_client.clone().get_version_info() {
+		// Isn't going to happen just yet (as of 2.0.0) but keep this here for
+		// the future. the nodeclient's get_version_info will return 1.0 if
+		// it gets a 404 for the version function
+		if Version::parse(&v.node_version) < Version::parse(MIN_COMPAT_NODE_VERSION) {
+			let version = if v.node_version == "1.0.0" {
+				"1.0.x series"
+			} else {
+				&v.node_version
+			};
+			println!("Specified Kepler Node (version {}) is outdated and incompatible with this wallet version", version);
+			println!("Please update the node or use a different one");
+			return 1;
 		}
-		// just continue if can't connect to node, as user won't be able to do
-		// anything meaninful anyhow
-		Err(_) => {}
 	}
+	// ... if node isn't available, allow offline functions
 
 	let res = wallet_args::wallet_command(wallet_args, wallet_config, node_client);
 