mimblewimble/grin-wallet

Add double rewind period (#155)
* Add double rewind period

* Simplify restore

* Fix comment

libwallet/src/internal/restore.rs
@@ -13,7 +13,7 @@
 // limitations under the License.
 //! Functions to restore a wallet's outputs from just the master seed
 
-use crate::grin_core::consensus::valid_header_version;
+use crate::grin_core::consensus::{valid_header_version, WEEK_HEIGHT};
 use crate::grin_core::core::HeaderVersion;
 use crate::grin_core::global;
 use crate::grin_core::libtx::proof;
@@ -23,6 +23,7 @@ use crate::internal::{keys, updater};
 use crate::types::*;
 use crate::{Error, OutputCommitMapping};
 use std::collections::HashMap;
+use std::time::Instant;
 
 /// Utility struct for return values from below
 #[derive(Clone)]
@@ -82,16 +83,29 @@ where
 		let (commit, proof, is_coinbase, height, mmr_index) = output;
 		// attempt to unwind message from the RP and get a value
 		// will fail if it's not ours
-		let info = if valid_header_version(*height, legacy_version) {
-			proof::rewind(keychain.secp(), &legacy_builder, *commit, None, *proof)
-		} else {
-			proof::rewind(keychain.secp(), &builder, *commit, None, *proof)
-		}?;
+		let info = {
+			// Before HF+2wk, try legacy rewind first
+			let info_legacy =
+				if valid_header_version(height.saturating_sub(2 * WEEK_HEIGHT), legacy_version) {
+					proof::rewind(keychain.secp(), &legacy_builder, *commit, None, *proof)?
+				} else {
+					None
+				};
 
-		if info.is_none() {
-			continue;
-		}
-		let (amount, key_id, switch) = info.unwrap();
+			// If legacy didn't work, try new rewind
+			if info_legacy.is_none() {
+				proof::rewind(keychain.secp(), &builder, *commit, None, *proof)?
+			} else {
+				info_legacy
+			}
+		};
+
+		let (amount, key_id, switch) = match info {
+			Some(i) => i,
+			None => {
+				continue;
+			}
+		};
 
 		let lock_height = if *is_coinbase {
 			*height + global::coinbase_maturity()
@@ -411,6 +425,7 @@ where
 		return Ok(());
 	}
 
+	let now = Instant::now();
 	warn!("Starting restore.");
 
 	let result_vec = collect_chain_outputs(wallet)?;
@@ -459,5 +474,11 @@ where
 		debug!("Next child for account {} is {}", path, max_child_index + 1);
 		batch.commit()?;
 	}
+
+	let mut sec = now.elapsed().as_secs();
+	let min = sec / 60;
+	sec %= 60;
+	info!("Restored wallet in {}m{}s", min, sec);
+
 	Ok(())
 }

libwallet/src/internal/updater.rs
@@ -19,10 +19,10 @@ use std::collections::HashMap;
 use uuid::Uuid;
 
 use crate::error::Error;
-use crate::grin_core::consensus::{reward, valid_header_version};
-use crate::grin_core::core::{HeaderVersion, Output, TxKernel};
+use crate::grin_core::consensus::reward;
+use crate::grin_core::core::{Output, TxKernel};
 use crate::grin_core::global;
-use crate::grin_core::libtx::proof::{LegacyProofBuilder, ProofBuilder};
+use crate::grin_core::libtx::proof::ProofBuilder;
 use crate::grin_core::libtx::reward;
 use crate::grin_keychain::{Identifier, Keychain, SwitchCommitmentType};
 use crate::grin_util as util;
@@ -511,12 +511,12 @@ where
 	debug!("receive_coinbase: {:?}", block_fees);
 
 	let keychain = wallet.keychain();
-	let (out, kern) = if valid_header_version(height, HeaderVersion(1)) {
-		let builder = LegacyProofBuilder::new(keychain);
-		reward::output(keychain, &builder, &key_id, block_fees.fees, test_mode)?
-	} else {
-		let builder = ProofBuilder::new(keychain);
-		reward::output(keychain, &builder, &key_id, block_fees.fees, test_mode)?
-	};
+	let (out, kern) = reward::output(
+		keychain,
+		&ProofBuilder::new(keychain),
+		&key_id,
+		block_fees.fees,
+		test_mode,
+	)?;
 	Ok((out, kern, block_fees))
 }

keplernetwork/kepler-wallet

libwallet/src/internal/restore.rs
@@ -14,7 +14,7 @@
 //! Functions to restore a wallet's outputs from just the master seed
 
 use crate::internal::{keys, updater};
-use crate::kepler_core::consensus::valid_header_version;
+use crate::kepler_core::consensus::{valid_header_version, WEEK_HEIGHT};
 use crate::kepler_core::core::HeaderVersion;
 use crate::kepler_core::global;
 use crate::kepler_core::libtx::proof;
@@ -23,6 +23,7 @@ use crate::kepler_util::secp::pedersen;
 use crate::types::*;
 use crate::{Error, OutputCommitMapping};
 use std::collections::HashMap;
+use std::time::Instant;
 
 /// Utility struct for return values from below
 #[derive(Clone)]
@@ -82,16 +83,29 @@ where
 		let (commit, proof, is_coinbase, height, mmr_index) = output;
 		// attempt to unwind message from the RP and get a value
 		// will fail if it's not ours
-		let info = if valid_header_version(*height, legacy_version) {
-			proof::rewind(keychain.secp(), &legacy_builder, *commit, None, *proof)
-		} else {
-			proof::rewind(keychain.secp(), &builder, *commit, None, *proof)
-		}?;
+		let info = {
+			// Before HF+2wk, try legacy rewind first
+			let info_legacy =
+				if valid_header_version(height.saturating_sub(2 * WEEK_HEIGHT), legacy_version) {
+					proof::rewind(keychain.secp(), &legacy_builder, *commit, None, *proof)?
+				} else {
+					None
+				};
 
-		if info.is_none() {
-			continue;
-		}
-		let (amount, key_id, switch) = info.unwrap();
+			// If legacy didn't work, try new rewind
+			if info_legacy.is_none() {
+				proof::rewind(keychain.secp(), &builder, *commit, None, *proof)?
+			} else {
+				info_legacy
+			}
+		};
+
+		let (amount, key_id, switch) = match info {
+			Some(i) => i,
+			None => {
+				continue;
+			}
+		};
 
 		let lock_height = if *is_coinbase {
 			*height + global::coinbase_maturity()
@@ -411,6 +425,7 @@ where
 		return Ok(());
 	}
 
+	let now = Instant::now();
 	warn!("Starting restore.");
 
 	let result_vec = collect_chain_outputs(wallet)?;
@@ -459,5 +474,11 @@ where
 		debug!("Next child for account {} is {}", path, max_child_index + 1);
 		batch.commit()?;
 	}
+
+	let mut sec = now.elapsed().as_secs();
+	let min = sec / 60;
+	sec %= 60;
+	info!("Restored wallet in {}m{}s", min, sec);
+
 	Ok(())
 }

libwallet/src/internal/updater.rs
@@ -20,10 +20,10 @@ use uuid::Uuid;
 
 use crate::error::Error;
 use crate::internal::keys;
-use crate::kepler_core::consensus::{reward, valid_header_version};
-use crate::kepler_core::core::{HeaderVersion, Output, TxKernel};
+use crate::kepler_core::consensus::reward;
+use crate::kepler_core::core::{Output, TxKernel};
 use crate::kepler_core::global;
-use crate::kepler_core::libtx::proof::{LegacyProofBuilder, ProofBuilder};
+use crate::kepler_core::libtx::proof::ProofBuilder;
 use crate::kepler_core::libtx::reward;
 use crate::kepler_keychain::{Identifier, Keychain, SwitchCommitmentType};
 use crate::kepler_util as util;
@@ -511,26 +511,13 @@ where
 	debug!("receive_coinbase: {:?}", block_fees);
 
 	let keychain = wallet.keychain();
-	let (out, kern) = if valid_header_version(height, HeaderVersion(1)) {
-		let builder = LegacyProofBuilder::new(keychain);
-		reward::output(
-			keychain,
-			&builder,
-			&key_id,
-			block_fees.fees,
-			height,
-			test_mode,
-		)?
-	} else {
-		let builder = ProofBuilder::new(keychain);
-		reward::output(
-			keychain,
-			&builder,
-			&key_id,
-			block_fees.fees,
-			height,
-			test_mode,
-		)?
-	};
+	let (out, kern) = reward::output(
+		keychain,
+		&ProofBuilder::new(keychain),
+		&key_id,
+		block_fees.fees,
+		height,
+		test_mode,
+	)?;
 	Ok((out, kern, block_fees))
 }