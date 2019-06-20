mimblewimble/grin-wallet

V0/V1 Slate Removal + Grin Header Version Bump (#140)
* Remove V0 and V1 Slates from Wallet

* rustfmt

libwallet/src/slate_versions/mod.rs
@@ -14,15 +14,12 @@
 
 //! This module contains old slate versions and conversions to the newest slate version
 //! Used for serialization and deserialization of slates in a backwards compatible way.
+//! Versions earlier than V2 are removed for the 2.0.0 release, but versioning code
+//! remains for future needs
+
 use crate::slate::Slate;
-use crate::slate_versions::v0::SlateV0;
-use crate::slate_versions::v1::SlateV1;
 use crate::slate_versions::v2::SlateV2;
 
-#[allow(missing_docs)]
-pub mod v0;
-#[allow(missing_docs)]
-pub mod v1;
 #[allow(missing_docs)]
 pub mod v2;
 
@@ -30,15 +27,11 @@ pub mod v2;
 pub const CURRENT_SLATE_VERSION: u16 = 2;
 
 /// The grin block header this slate is intended to be compatible with
-pub const GRIN_BLOCK_HEADER_VERSION: u16 = 1;
+pub const GRIN_BLOCK_HEADER_VERSION: u16 = 2;
 
 /// Existing versions of the slate
 #[derive(EnumIter, Serialize, Deserialize, Clone, Debug, PartialEq, PartialOrd, Eq, Ord)]
 pub enum SlateVersion {
-	/// V0
-	V0,
-	/// V1
-	V1,
 	/// V2 (most current)
 	V2,
 }
@@ -48,12 +41,8 @@ pub enum SlateVersion {
 /// Versions are ordered newest to oldest so serde attempts to
 /// deserialize newer versions first, then falls back to older versions.
 pub enum VersionedSlate {
-	/// Current
+	/// Current (Grin 1.1.0 - 2.x (current))
 	V2(SlateV2),
-	/// V1 - Grin 1.0.1 - 1.0.3)
-	V1(SlateV1),
-	/// V0 - Grin 1.0.0
-	V0(SlateV0),
 }
 
 impl VersionedSlate {
@@ -61,8 +50,6 @@ impl VersionedSlate {
 	pub fn version(&self) -> SlateVersion {
 		match *self {
 			VersionedSlate::V2(_) => SlateVersion::V2,
-			VersionedSlate::V1(_) => SlateVersion::V1,
-			VersionedSlate::V0(_) => SlateVersion::V0,
 		}
 	}
 
@@ -70,17 +57,14 @@ impl VersionedSlate {
 	pub fn into_version(slate: Slate, version: SlateVersion) -> VersionedSlate {
 		match version {
 			SlateVersion::V2 => VersionedSlate::V2(slate.into()),
-			SlateVersion::V1 => {
-				let s = SlateV2::from(slate);
-				let s = SlateV1::from(s);
-				VersionedSlate::V1(s)
-			}
-			SlateVersion::V0 => {
+			// Left here as a reminder of what needs to be inserted on
+			// the release of a new slate
+			/*SlateVersion::V0 => {
 				let s = SlateV2::from(slate);
 				let s = SlateV1::from(s);
 				let s = SlateV0::from(s);
 				VersionedSlate::V0(s)
-			}
+			}*/
 		}
 	}
 }
@@ -91,18 +75,13 @@ impl From<VersionedSlate> for Slate {
 			VersionedSlate::V2(s) => {
 				let s = SlateV2::from(s);
 				Slate::from(s)
-			}
-			VersionedSlate::V1(s) => {
-				let s = SlateV1::from(s);
-				let s = SlateV2::from(s);
-				Slate::from(s)
-			}
-			VersionedSlate::V0(s) => {
-				let s = SlateV0::from(s);
-				let s = SlateV1::from(s);
-				let s = SlateV2::from(s);
-				Slate::from(s)
-			}
+			} // Again, left in as a reminder
+			  /*VersionedSlate::V0(s) => {
+				  let s = SlateV0::from(s);
+				  let s = SlateV1::from(s);
+				  let s = SlateV2::from(s);
+				  Slate::from(s)
+			  }*/
 		}
 	}
 }

keplernetwork/kepler-wallet

libwallet/src/slate_versions/mod.rs
@@ -14,15 +14,12 @@
 
 //! This module contains old slate versions and conversions to the newest slate version
 //! Used for serialization and deserialization of slates in a backwards compatible way.
+//! Versions earlier than V2 are removed for the 2.0.0 release, but versioning code
+//! remains for future needs
+
 use crate::slate::Slate;
-use crate::slate_versions::v0::SlateV0;
-use crate::slate_versions::v1::SlateV1;
 use crate::slate_versions::v2::SlateV2;
 
-#[allow(missing_docs)]
-pub mod v0;
-#[allow(missing_docs)]
-pub mod v1;
 #[allow(missing_docs)]
 pub mod v2;
 
@@ -30,15 +27,11 @@ pub mod v2;
 pub const CURRENT_SLATE_VERSION: u16 = 2;
 
 /// The kepler block header this slate is intended to be compatible with
-pub const KEPLER_BLOCK_HEADER_VERSION: u16 = 1;
+pub const KEPLER_BLOCK_HEADER_VERSION: u16 = 2;
 
 /// Existing versions of the slate
 #[derive(EnumIter, Serialize, Deserialize, Clone, Debug, PartialEq, PartialOrd, Eq, Ord)]
 pub enum SlateVersion {
-	/// V0
-	V0,
-	/// V1
-	V1,
 	/// V2 (most current)
 	V2,
 }
@@ -50,10 +43,6 @@ pub enum SlateVersion {
 pub enum VersionedSlate {
 	/// Current
 	V2(SlateV2),
-	/// V1 - Kepler 1.0.1 - 1.0.3)
-	V1(SlateV1),
-	/// V0 - Kepler 1.0.0
-	V0(SlateV0),
 }
 
 impl VersionedSlate {
@@ -61,8 +50,6 @@ impl VersionedSlate {
 	pub fn version(&self) -> SlateVersion {
 		match *self {
 			VersionedSlate::V2(_) => SlateVersion::V2,
-			VersionedSlate::V1(_) => SlateVersion::V1,
-			VersionedSlate::V0(_) => SlateVersion::V0,
 		}
 	}
 
@@ -70,17 +57,14 @@ impl VersionedSlate {
 	pub fn into_version(slate: Slate, version: SlateVersion) -> VersionedSlate {
 		match version {
 			SlateVersion::V2 => VersionedSlate::V2(slate.into()),
-			SlateVersion::V1 => {
-				let s = SlateV2::from(slate);
-				let s = SlateV1::from(s);
-				VersionedSlate::V1(s)
-			}
-			SlateVersion::V0 => {
+			// Left here as a reminder of what needs to be inserted on
+			// the release of a new slate
+			/*SlateVersion::V0 => {
 				let s = SlateV2::from(slate);
 				let s = SlateV1::from(s);
 				let s = SlateV0::from(s);
 				VersionedSlate::V0(s)
-			}
+			}*/
 		}
 	}
 }
@@ -91,18 +75,13 @@ impl From<VersionedSlate> for Slate {
 			VersionedSlate::V2(s) => {
 				let s = SlateV2::from(s);
 				Slate::from(s)
-			}
-			VersionedSlate::V1(s) => {
-				let s = SlateV1::from(s);
-				let s = SlateV2::from(s);
-				Slate::from(s)
-			}
-			VersionedSlate::V0(s) => {
-				let s = SlateV0::from(s);
-				let s = SlateV1::from(s);
-				let s = SlateV2::from(s);
-				Slate::from(s)
-			}
+			} // Again, left in as a reminder
+			  /*VersionedSlate::V0(s) => {
+				  let s = SlateV0::from(s);
+				  let s = SlateV1::from(s);
+				  let s = SlateV2::from(s);
+				  Slate::from(s)
+			  }*/
 		}
 	}
 }