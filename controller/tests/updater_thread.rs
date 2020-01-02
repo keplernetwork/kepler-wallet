mimblewimble/grin-wallet

Add updater thread + Updater related functions (#253)
* first attempt at adding updater thread

* rustfmt

* rustfmt

* many lifetimes made static to allow api to spawn separate thread

* add first pass at updater thread functionality

* rustfmt

* add mpsc for returning status from update functions

* rustfmt

* add stop state, ensure update is triggered by wallet functions when wallet updater is running

* rustfmt

* add update status function to owner API, V3 owner api functions

* rustfmt

* change update warning

* adding tests for new updater control functions

* documentation updates

* rustfmt

controller/tests/updater_thread.rs
@@ -1,0 +1,122 @@
+// Copyright 2019 The Grin Developers
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Test a wallet repost command
+#[macro_use]
+extern crate log;
+extern crate grin_wallet_api as api;
+extern crate grin_wallet_controller as wallet;
+extern crate grin_wallet_impls as impls;
+extern crate grin_wallet_libwallet as libwallet;
+
+use crate::libwallet::api_impl::owner_updater::{start_updater_log_thread, StatusMessage};
+use grin_wallet_util::grin_core as core;
+
+use self::libwallet::{InitTxArgs, Slate};
+use impls::test_framework::{self, LocalWalletClient};
+use impls::{PathToSlate, SlateGetter as _, SlatePutter as _};
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
+#[macro_use]
+mod common;
+use common::{clean_output_dir, create_wallet_proxy, setup};
+
+/// updater thread test impl
+fn updater_thread_test_impl(test_dir: &'static str) -> Result<(), libwallet::Error> {
+	// Create a new proxy to simulate server and wallet responses
+	let mut wallet_proxy = create_wallet_proxy(test_dir);
+	let chain = wallet_proxy.chain.clone();
+
+	// Create a new wallet test client, and set its queues to communicate with the
+	// proxy
+	create_wallet_and_add!(
+		client1,
+		wallet1,
+		mask1_i,
+		test_dir,
+		"wallet1",
+		None,
+		&mut wallet_proxy,
+		false
+	);
+	let mask1 = (&mask1_i).as_ref();
+	create_wallet_and_add!(
+		client2,
+		wallet2,
+		mask2_i,
+		test_dir,
+		"wallet2",
+		None,
+		&mut wallet_proxy,
+		false
+	);
+	let mask2 = (&mask2_i).as_ref();
+
+	// Set the wallet proxy listener running
+	thread::spawn(move || {
+		if let Err(e) = wallet_proxy.run() {
+			error!("Wallet Proxy error: {}", e);
+		}
+	});
+
+	// few values to keep things shorter
+	let reward = core::consensus::REWARD;
+
+	// add some accounts
+	wallet::controller::owner_single_use(wallet1.clone(), mask1, |api, m| {
+		api.create_account_path(m, "mining")?;
+		api.create_account_path(m, "listener")?;
+		Ok(())
+	})?;
+
+	// add some accounts
+	wallet::controller::owner_single_use(wallet2.clone(), mask2, |api, m| {
+		api.create_account_path(m, "account1")?;
+		api.create_account_path(m, "account2")?;
+		Ok(())
+	})?;
+
+	// Get some mining done
+	{
+		wallet_inst!(wallet1, w);
+		w.set_parent_key_id_by_name("mining")?;
+	}
+	let mut bh = 10u64;
+	let _ =
+		test_framework::award_blocks_to_wallet(&chain, wallet1.clone(), mask1, bh as usize, false);
+
+	let owner_api = api::Owner::new(wallet1);
+	owner_api.start_updater(mask1, Duration::from_secs(5))?;
+
+	// let updater thread run a bit
+	thread::sleep(Duration::from_secs(10));
+
+	let messages = owner_api.get_updater_messages(1000)?;
+	assert_eq!(messages.len(), 34);
+
+	owner_api.stop_updater()?;
+	thread::sleep(Duration::from_secs(2));
+	Ok(())
+}
+
+#[test]
+fn updater_thread() {
+	let test_dir = "test_output/updater_thread";
+	setup(test_dir);
+	if let Err(e) = updater_thread_test_impl(test_dir) {
+		panic!("Libwallet Error: {} - {}", e, e.backtrace().unwrap());
+	}
+	clean_output_dir(test_dir);
+}

libwallet/src/api_impl/owner_updater.rs
@@ -1,0 +1,140 @@
+// Copyright 2019 The Grin Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A threaded persistent Updater that can be controlled by a grin wallet
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use crate::grin_keychain::Keychain;
+use crate::grin_util::secp::key::SecretKey;
+use crate::grin_util::Mutex;
+
+use crate::api_impl::owner;
+use crate::types::NodeClient;
+use crate::Error;
+use crate::{WalletInst, WalletLCProvider};
+
+const MESSAGE_QUEUE_MAX_LEN: usize = 10_000;
+
+/// Update status messages which can be returned to listening clients
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum StatusMessage {
+	/// Wallet is performing a regular update, matching the UTXO set against
+	/// current wallet outputs
+	UpdatingOutputs(String),
+	/// Wallet is updating transactions, potentially retrieving transactions
+	/// by kernel if needed
+	UpdatingTransactions(String),
+	/// Warning that the wallet is about to perform a full UTXO scan
+	FullScanWarn(String),
+	/// Status and percentage complete messages returned during the
+	/// scanning process
+	Scanning(String, u8),
+	/// UTXO scanning is complete
+	ScanningComplete(String),
+	/// Warning of issues that may have occured during an update
+	UpdateWarning(String),
+}
+
+/// Helper function that starts a simple log thread for updater messages
+pub fn start_updater_log_thread(
+	rx: Receiver<StatusMessage>,
+	queue: Arc<Mutex<Vec<StatusMessage>>>,
+) -> Result<(), Error> {
+	let _ = thread::Builder::new()
+		.name("wallet-updater-status".to_string())
+		.spawn(move || loop {
+			while let Ok(m) = rx.try_recv() {
+				// save to our message queue to be read by other consumers
+				{
+					let mut q = queue.lock();
+					q.insert(0, m.clone());
+					while q.len() > MESSAGE_QUEUE_MAX_LEN {
+						q.pop();
+					}
+				}
+				match m {
+					StatusMessage::UpdatingOutputs(s) => debug!("{}", s),
+					StatusMessage::UpdatingTransactions(s) => debug!("{}", s),
+					StatusMessage::FullScanWarn(s) => warn!("{}", s),
+					StatusMessage::Scanning(s, m) => {
+						debug!("{}", s);
+						warn!("Scanning - {}% complete", m);
+					}
+					StatusMessage::ScanningComplete(s) => warn!("{}", s),
+					StatusMessage::UpdateWarning(s) => warn!("{}", s),
+				}
+			}
+			thread::sleep(Duration::from_millis(500));
+		})?;
+
+	Ok(())
+}
+
+/// Handles and launches a background update thread
+pub struct Updater<'a, L, C, K>
+where
+	L: WalletLCProvider<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	wallet_inst: Arc<Mutex<Box<dyn WalletInst<'a, L, C, K>>>>,
+	is_running: Arc<AtomicBool>,
+}
+
+impl<'a, L, C, K> Updater<'a, L, C, K>
+where
+	L: WalletLCProvider<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	/// create a new updater
+	pub fn new(
+		wallet_inst: Arc<Mutex<Box<dyn WalletInst<'a, L, C, K>>>>,
+		is_running: Arc<AtomicBool>,
+	) -> Self {
+		is_running.store(false, Ordering::Relaxed);
+		Updater {
+			wallet_inst,
+			is_running,
+		}
+	}
+
+	/// Start the updater at the given frequency
+	pub fn run(
+		&self,
+		frequency: Duration,
+		keychain_mask: Option<SecretKey>,
+		status_send_channel: &Option<Sender<StatusMessage>>,
+	) -> Result<(), Error> {
+		self.is_running.store(true, Ordering::Relaxed);
+		loop {
+			// Business goes here
+			owner::update_wallet_state(
+				self.wallet_inst.clone(),
+				(&keychain_mask).as_ref(),
+				status_send_channel,
+				false,
+			)?;
+			if !self.is_running.load(Ordering::Relaxed) {
+				break;
+			}
+			thread::sleep(frequency);
+		}
+		Ok(())
+	}
+}

keplernetwork/kepler-wallet

controller/tests/updater_thread.rs
@@ -1,0 +1,122 @@
+// Copyright 2019 The Grin Developers
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Test a wallet repost command
+#[macro_use]
+extern crate log;
+extern crate kepler_wallet_api as api;
+extern crate kepler_wallet_controller as wallet;
+extern crate kepler_wallet_impls as impls;
+extern crate kepler_wallet_libwallet as libwallet;
+
+use crate::libwallet::api_impl::owner_updater::{start_updater_log_thread, StatusMessage};
+use kepler_wallet_util::kepler_core as core;
+
+use self::libwallet::{InitTxArgs, Slate};
+use impls::test_framework::{self, LocalWalletClient};
+use impls::{PathToSlate, SlateGetter as _, SlatePutter as _};
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
+#[macro_use]
+mod common;
+use common::{clean_output_dir, create_wallet_proxy, setup};
+
+/// updater thread test impl
+fn updater_thread_test_impl(test_dir: &'static str) -> Result<(), libwallet::Error> {
+	// Create a new proxy to simulate server and wallet responses
+	let mut wallet_proxy = create_wallet_proxy(test_dir);
+	let chain = wallet_proxy.chain.clone();
+
+	// Create a new wallet test client, and set its queues to communicate with the
+	// proxy
+	create_wallet_and_add!(
+		client1,
+		wallet1,
+		mask1_i,
+		test_dir,
+		"wallet1",
+		None,
+		&mut wallet_proxy,
+		false
+	);
+	let mask1 = (&mask1_i).as_ref();
+	create_wallet_and_add!(
+		client2,
+		wallet2,
+		mask2_i,
+		test_dir,
+		"wallet2",
+		None,
+		&mut wallet_proxy,
+		false
+	);
+	let mask2 = (&mask2_i).as_ref();
+
+	// Set the wallet proxy listener running
+	thread::spawn(move || {
+		if let Err(e) = wallet_proxy.run() {
+			error!("Wallet Proxy error: {}", e);
+		}
+	});
+
+	// few values to keep things shorter
+	let reward = core::consensus::REWARD;
+
+	// add some accounts
+	wallet::controller::owner_single_use(wallet1.clone(), mask1, |api, m| {
+		api.create_account_path(m, "mining")?;
+		api.create_account_path(m, "listener")?;
+		Ok(())
+	})?;
+
+	// add some accounts
+	wallet::controller::owner_single_use(wallet2.clone(), mask2, |api, m| {
+		api.create_account_path(m, "account1")?;
+		api.create_account_path(m, "account2")?;
+		Ok(())
+	})?;
+
+	// Get some mining done
+	{
+		wallet_inst!(wallet1, w);
+		w.set_parent_key_id_by_name("mining")?;
+	}
+	let mut bh = 10u64;
+	let _ =
+		test_framework::award_blocks_to_wallet(&chain, wallet1.clone(), mask1, bh as usize, false);
+
+	let owner_api = api::Owner::new(wallet1);
+	owner_api.start_updater(mask1, Duration::from_secs(5))?;
+
+	// let updater thread run a bit
+	thread::sleep(Duration::from_secs(10));
+
+	let messages = owner_api.get_updater_messages(1000)?;
+	assert_eq!(messages.len(), 34);
+
+	owner_api.stop_updater()?;
+	thread::sleep(Duration::from_secs(2));
+	Ok(())
+}
+
+#[test]
+fn updater_thread() {
+	let test_dir = "test_output/updater_thread";
+	setup(test_dir);
+	if let Err(e) = updater_thread_test_impl(test_dir) {
+		panic!("Libwallet Error: {} - {}", e, e.backtrace().unwrap());
+	}
+	clean_output_dir(test_dir);
+}

libwallet/src/api_impl/owner_updater.rs
@@ -1,0 +1,140 @@
+// Copyright 2019 The Grin Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A threaded persistent Updater that can be controlled by a kepler wallet
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use crate::kepler_keychain::Keychain;
+use crate::kepler_util::secp::key::SecretKey;
+use crate::kepler_util::Mutex;
+
+use crate::api_impl::owner;
+use crate::types::NodeClient;
+use crate::Error;
+use crate::{WalletInst, WalletLCProvider};
+
+const MESSAGE_QUEUE_MAX_LEN: usize = 10_000;
+
+/// Update status messages which can be returned to listening clients
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum StatusMessage {
+	/// Wallet is performing a regular update, matching the UTXO set against
+	/// current wallet outputs
+	UpdatingOutputs(String),
+	/// Wallet is updating transactions, potentially retrieving transactions
+	/// by kernel if needed
+	UpdatingTransactions(String),
+	/// Warning that the wallet is about to perform a full UTXO scan
+	FullScanWarn(String),
+	/// Status and percentage complete messages returned during the
+	/// scanning process
+	Scanning(String, u8),
+	/// UTXO scanning is complete
+	ScanningComplete(String),
+	/// Warning of issues that may have occured during an update
+	UpdateWarning(String),
+}
+
+/// Helper function that starts a simple log thread for updater messages
+pub fn start_updater_log_thread(
+	rx: Receiver<StatusMessage>,
+	queue: Arc<Mutex<Vec<StatusMessage>>>,
+) -> Result<(), Error> {
+	let _ = thread::Builder::new()
+		.name("wallet-updater-status".to_string())
+		.spawn(move || loop {
+			while let Ok(m) = rx.try_recv() {
+				// save to our message queue to be read by other consumers
+				{
+					let mut q = queue.lock();
+					q.insert(0, m.clone());
+					while q.len() > MESSAGE_QUEUE_MAX_LEN {
+						q.pop();
+					}
+				}
+				match m {
+					StatusMessage::UpdatingOutputs(s) => debug!("{}", s),
+					StatusMessage::UpdatingTransactions(s) => debug!("{}", s),
+					StatusMessage::FullScanWarn(s) => warn!("{}", s),
+					StatusMessage::Scanning(s, m) => {
+						debug!("{}", s);
+						warn!("Scanning - {}% complete", m);
+					}
+					StatusMessage::ScanningComplete(s) => warn!("{}", s),
+					StatusMessage::UpdateWarning(s) => warn!("{}", s),
+				}
+			}
+			thread::sleep(Duration::from_millis(500));
+		})?;
+
+	Ok(())
+}
+
+/// Handles and launches a background update thread
+pub struct Updater<'a, L, C, K>
+where
+	L: WalletLCProvider<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	wallet_inst: Arc<Mutex<Box<dyn WalletInst<'a, L, C, K>>>>,
+	is_running: Arc<AtomicBool>,
+}
+
+impl<'a, L, C, K> Updater<'a, L, C, K>
+where
+	L: WalletLCProvider<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	/// create a new updater
+	pub fn new(
+		wallet_inst: Arc<Mutex<Box<dyn WalletInst<'a, L, C, K>>>>,
+		is_running: Arc<AtomicBool>,
+	) -> Self {
+		is_running.store(false, Ordering::Relaxed);
+		Updater {
+			wallet_inst,
+			is_running,
+		}
+	}
+
+	/// Start the updater at the given frequency
+	pub fn run(
+		&self,
+		frequency: Duration,
+		keychain_mask: Option<SecretKey>,
+		status_send_channel: &Option<Sender<StatusMessage>>,
+	) -> Result<(), Error> {
+		self.is_running.store(true, Ordering::Relaxed);
+		loop {
+			// Business goes here
+			owner::update_wallet_state(
+				self.wallet_inst.clone(),
+				(&keychain_mask).as_ref(),
+				status_send_channel,
+				false,
+			)?;
+			if !self.is_running.load(Ordering::Relaxed) {
+				break;
+			}
+			thread::sleep(frequency);
+		}
+		Ok(())
+	}
+}