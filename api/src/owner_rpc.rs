mimblewimble/grin-wallet

Bits and Pieces for 3.0.0 beta (#271)
* deprecate the V2 Owner API

* rustfmt

* fix output from scan

* fix scan output to be more consistent

* rustfmt

* updater thread test

api/src/owner_rpc.rs
@@ -67,6 +67,10 @@ pub trait OwnerRpc: Sync + Send {
 	# , false, 4, false, false, false);
 	```
 	*/
+	#[deprecated(
+		since = "3.0.0",
+		note = "The V2 Owner API (OwnerRpc) will be removed in grin-wallet 4.0.0. Please migrate to the V3 (OwnerRpcS) API as soon as possible."
+	)]
 	fn accounts(&self) -> Result<Vec<AcctPathMapping>, ErrorKind>;
 
 	/**

keplernetwork/kepler-wallet

api/src/owner_rpc.rs
@@ -67,6 +67,10 @@ pub trait OwnerRpc: Sync + Send {
 	# , false, 4, false, false, false);
 	```
 	*/
+	#[deprecated(
+		since = "3.0.0",
+		note = "The V2 Owner API (OwnerRpc) will be removed in kepler-wallet 4.0.0. Please migrate to the V3 (OwnerRpcS) API as soon as possible."
+	)]
 	fn accounts(&self) -> Result<Vec<AcctPathMapping>, ErrorKind>;
 
 	/**