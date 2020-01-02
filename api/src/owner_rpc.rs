mimblewimble/grin-wallet

Add `init_api_secure` function (#206)
* adding initial version of init_secure_api

* rustfmt

* fix ECDH algo

* rustfmt

* trying to figure out best way of doing encryption

* refactor secure requests and responses into json-rpc responses, with base64 payload for encrypted messages

* rustfmt

* return proper errors from encrypted api, include tests covering encrypted API error cases

* rustfmt

* add test for normal error (unencrypted)

* rustfmt

* change ports for test, add foreign listener to V2 sanity tests, add ability to select owner api port via command line

* rustfmt

* turn it to 11

* explicit teardown after rpc tests

* update tests with explicit teardowns

* update tests to perform explicit teardown

* fix warnings, ensure all tests teardown

* log output to diagnose CI windows build failures

* disable owner api doctests on windows

* rustfmt

api/src/owner_rpc.rs
@@ -24,15 +24,15 @@ use crate::libwallet::{
 };
 use crate::util::Mutex;
 use crate::{Owner, OwnerRpcS};
-use easy_jsonrpc;
+use easy_jsonrpc_mw;
 use std::sync::Arc;
 
 /// Public definition used to generate Owner jsonrpc api.
 /// * When running `grin-wallet owner_api` with defaults, the V2 api is available at
 /// `localhost:3420/v2/owner`
 /// * The endpoint only supports POST operations, with the json-rpc request as the body
-#[easy_jsonrpc::rpc]
-pub trait OwnerRpc {
+#[easy_jsonrpc_mw::rpc]
+pub trait OwnerRpc: Sync + Send {
 	/**
 	Networked version of [Owner::accounts](struct.Owner.html#method.accounts).
 
@@ -1148,7 +1148,7 @@ pub trait OwnerRpc {
 		}
 	}
 	# "#
-	# ,false, 5 ,true, false, false);
+	# ,false, 0 ,false, false, false);
 	```
 	*/
 	fn verify_slate_messages(&self, slate: VersionedSlate) -> Result<(), ErrorKind>;
@@ -1370,7 +1370,7 @@ pub fn run_doctest_owner(
 	lock_tx: bool,
 	finalize_tx: bool,
 ) -> Result<Option<serde_json::Value>, String> {
-	use easy_jsonrpc::Handler;
+	use easy_jsonrpc_mw::Handler;
 	use grin_wallet_impls::test_framework::{self, LocalWalletClient, WalletProxy};
 	use grin_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
 	use grin_wallet_libwallet::{api_impl, WalletInst};
@@ -1404,7 +1404,7 @@ pub fn run_doctest_owner(
 	let mut wallet1 =
 		Box::new(DefaultWalletImpl::<LocalWalletClient>::new(client1.clone()).unwrap())
 			as Box<
-				WalletInst<
+				dyn WalletInst<
 					'static,
 					DefaultLCProvider<LocalWalletClient, ExtKeychain>,
 					LocalWalletClient,
@@ -1439,7 +1439,7 @@ pub fn run_doctest_owner(
 	let mut wallet2 =
 		Box::new(DefaultWalletImpl::<LocalWalletClient>::new(client2.clone()).unwrap())
 			as Box<
-				WalletInst<
+				dyn WalletInst<
 					'static,
 					DefaultLCProvider<LocalWalletClient, ExtKeychain>,
 					LocalWalletClient,
@@ -1547,13 +1547,15 @@ pub fn run_doctest_owner(
 
 	let mut api_owner = Owner::new(wallet1);
 	api_owner.doctest_mode = true;
-	if use_token {
+	let res = if use_token {
 		let owner_api = &api_owner as &dyn OwnerRpcS;
-		Ok(owner_api.handle_request(request).as_option())
+		owner_api.handle_request(request).as_option()
 	} else {
 		let owner_api = &api_owner as &dyn OwnerRpc;
-		Ok(owner_api.handle_request(request).as_option())
-	}
+		owner_api.handle_request(request).as_option()
+	};
+	let _ = fs::remove_dir_all(test_dir);
+	Ok(res)
 }
 
 #[doc(hidden)]
@@ -1563,39 +1565,46 @@ macro_rules! doctest_helper_json_rpc_owner_assert_response {
 		// create temporary wallet, run jsonrpc request on owner api of wallet, delete wallet, return
 		// json response.
 		// In order to prevent leaking tempdirs, This function should not panic.
-		use grin_wallet_api::run_doctest_owner;
-		use serde_json;
-		use serde_json::Value;
-		use tempfile::tempdir;
 
-		let dir = tempdir().map_err(|e| format!("{:#?}", e)).unwrap();
-		let dir = dir
-			.path()
-			.to_str()
-			.ok_or("Failed to convert tmpdir path to string.".to_owned())
+		// These cause LMDB to run out of disk space on CircleCI
+		// disable for now on windows
+		// TODO: Fix properly
+		#[cfg(not(target_os = "windows"))]
+			{
+			use grin_wallet_api::run_doctest_owner;
+			use serde_json;
+			use serde_json::Value;
+			use tempfile::tempdir;
+
+			let dir = tempdir().map_err(|e| format!("{:#?}", e)).unwrap();
+			let dir = dir
+				.path()
+				.to_str()
+				.ok_or("Failed to convert tmpdir path to string.".to_owned())
+				.unwrap();
+
+			let request_val: Value = serde_json::from_str($request).unwrap();
+			let expected_response: Value = serde_json::from_str($expected_response).unwrap();
+
+			let response = run_doctest_owner(
+				request_val,
+				dir,
+				$use_token,
+				$blocks_to_mine,
+				$perform_tx,
+				$lock_tx,
+				$finalize_tx,
+				)
+			.unwrap()
 			.unwrap();
 
-		let request_val: Value = serde_json::from_str($request).unwrap();
-		let expected_response: Value = serde_json::from_str($expected_response).unwrap();
-
-		let response = run_doctest_owner(
-			request_val,
-			dir,
-			$use_token,
-			$blocks_to_mine,
-			$perform_tx,
-			$lock_tx,
-			$finalize_tx,
-			)
-		.unwrap()
-		.unwrap();
-
-		if response != expected_response {
-			panic!(
-				"(left != right) \nleft: {}\nright: {}",
-				serde_json::to_string_pretty(&response).unwrap(),
-				serde_json::to_string_pretty(&expected_response).unwrap()
+			if response != expected_response {
+				panic!(
+					"(left != right) \nleft: {}\nright: {}",
+					serde_json::to_string_pretty(&response).unwrap(),
+					serde_json::to_string_pretty(&expected_response).unwrap()
 				);
+				}
 			}
 	};
 }

tests/common/mod.rs
@@ -14,6 +14,7 @@
 //! Common functions for wallet integration tests
 extern crate grin_wallet;
 
+use grin_wallet_config as config;
 use grin_wallet_impls::test_framework::LocalWalletClient;
 use grin_wallet_util::grin_util as util;
 
@@ -23,12 +24,14 @@ use std::sync::Arc;
 use std::{env, fs};
 use util::{Mutex, ZeroingString};
 
+use grin_wallet_api::{EncryptedRequest, EncryptedResponse};
 use grin_wallet_config::{GlobalWalletConfig, WalletConfig, GRIN_WALLET_DIR};
 use grin_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
 use grin_wallet_libwallet::{WalletInfo, WalletInst};
 use grin_wallet_util::grin_core::global::{self, ChainTypes};
 use grin_wallet_util::grin_keychain::ExtKeychain;
-use util::secp::key::SecretKey;
+use grin_wallet_util::grin_util::{from_hex, static_secp_instance};
+use util::secp::key::{PublicKey, SecretKey};
 
 use grin_wallet::cmd::wallet_args;
 use grin_wallet_util::grin_api as api;
@@ -100,7 +103,7 @@ macro_rules! setup_proxy {
 	};
 }
 
-fn clean_output_dir(test_dir: &str) {
+pub fn clean_output_dir(test_dir: &str) {
 	let _ = fs::remove_dir_all(test_dir);
 }
 
@@ -192,7 +195,7 @@ pub fn instantiate_wallet(
 		Arc<
 			Mutex<
 				Box<
-					WalletInst<
+					dyn WalletInst<
 						'static,
 						DefaultLCProvider<'static, LocalWalletClient, ExtKeychain>,
 						LocalWalletClient,
@@ -208,7 +211,7 @@ pub fn instantiate_wallet(
 	wallet_config.chain_type = None;
 	let mut wallet = Box::new(DefaultWalletImpl::<LocalWalletClient>::new(node_client).unwrap())
 		as Box<
-			WalletInst<
+			dyn WalletInst<
 				DefaultLCProvider<'static, LocalWalletClient, ExtKeychain>,
 				LocalWalletClient,
 				ExtKeychain,
@@ -247,6 +250,25 @@ pub fn execute_command(
 	wallet_args::wallet_command(&args, config.clone(), client.clone(), true)
 }
 
+// as above, but without necessarily setting up the wallet
+#[allow(dead_code)]
+pub fn execute_command_no_setup(
+	app: &App,
+	test_dir: &str,
+	wallet_name: &str,
+	client: &LocalWalletClient,
+	arg_vec: Vec<&str>,
+) -> Result<String, grin_wallet_controller::Error> {
+	let args = app.clone().get_matches_from(arg_vec);
+	let _ = get_wallet_subcommand(test_dir, wallet_name, args.clone());
+	let config = config::initial_setup_wallet(&ChainTypes::AutomatedTesting, None).unwrap();
+	let mut wallet_config = config.members.unwrap().wallet.clone();
+	wallet_config.chain_type = None;
+	wallet_config.api_secret_path = None;
+	wallet_config.node_api_secret_path = None;
+	wallet_args::wallet_command(&args, wallet_config, client.clone(), true)
+}
+
 pub fn post<IN>(url: &Url, api_secret: Option<String>, input: &IN) -> Result<String, api::Error>
 where
 	IN: Serialize,
@@ -257,6 +279,7 @@ where
 	Ok(res)
 }
 
+#[allow(dead_code)]
 pub fn send_request<OUT>(
 	id: u64,
 	dest: &str,
@@ -266,19 +289,30 @@ where
 	OUT: DeserializeOwned,
 {
 	let url = Url::parse(dest).unwrap();
-	let req: Value = serde_json::from_str(req).unwrap();
-	let res: String = post(&url, None, &req).map_err(|e| {
+	let req_val: Value = serde_json::from_str(req).unwrap();
+	let res = post(&url, None, &req_val).map_err(|e| {
 		let err_string = format!("{}", e);
 		println!("{}", err_string);
 		thread::sleep(Duration::from_millis(200));
 		e
 	})?;
+
+	let res_val: Value = serde_json::from_str(&res).unwrap();
+	// encryption error, just return the string
+	if res_val["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res_val["error"]["message"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+
 	let res = serde_json::from_str(&res).unwrap();
 	let res = easy_jsonrpc::Response::from_json_response(res).unwrap();
 	let res = res.outputs.get(&id).unwrap().clone().unwrap();
 	if res["Err"] != json!(null) {
 		Ok(Err(WalletAPIReturnError {
 			message: res["Err"].as_str().unwrap().to_owned(),
+			code: res["error"]["code"].as_i64().unwrap() as i32,
 		}))
 	} else {
 		// deserialize result into expected type
@@ -287,10 +321,88 @@ where
 	}
 }
 
+#[allow(dead_code)]
+pub fn send_request_enc<OUT>(
+	sec_req_id: u32,
+	internal_request_id: u32,
+	dest: &str,
+	req: &str,
+	shared_key: &SecretKey,
+) -> Result<Result<OUT, WalletAPIReturnError>, api::Error>
+where
+	OUT: DeserializeOwned,
+{
+	let url = Url::parse(dest).unwrap();
+	let req_val: Value = serde_json::from_str(req).unwrap();
+	let req = EncryptedRequest::from_json(sec_req_id, &req_val, &shared_key).unwrap();
+	let res = post(&url, None, &req).map_err(|e| {
+		let err_string = format!("{}", e);
+		println!("{}", err_string);
+		thread::sleep(Duration::from_millis(200));
+		e
+	})?;
+
+	let res_val: Value = serde_json::from_str(&res).unwrap();
+	// encryption error, just return the string
+	if res_val["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res_val["error"]["message"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+
+	let enc_resp: EncryptedResponse = serde_json::from_str(&res).unwrap();
+	let res = enc_resp.decrypt(shared_key).unwrap();
+	if res["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res["error"]["message"].as_str().unwrap().to_owned(),
+			code: res["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+	let res = easy_jsonrpc::Response::from_json_response(res).unwrap();
+	let res = res
+		.outputs
+		.get(&(internal_request_id as u64))
+		.unwrap()
+		.clone()
+		.unwrap();
+
+	if res["Err"] != json!(null) {
+		Ok(Err(WalletAPIReturnError {
+			message: res["Err"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}))
+	} else {
+		// deserialize result into expected type
+		let value: OUT = serde_json::from_value(res["Ok"].clone()).unwrap();
+		Ok(Ok(value))
+	}
+}
+
+#[allow(dead_code)]
+pub fn derive_ecdh_key(sec_key_str: &str, other_pubkey: &PublicKey) -> SecretKey {
+	let sec_key_bytes = from_hex(sec_key_str.to_owned()).unwrap();
+	let sec_key = {
+		let secp_inst = static_secp_instance();
+		let secp = secp_inst.lock();
+		SecretKey::from_slice(&secp, &sec_key_bytes).unwrap()
+	};
+
+	let secp_inst = static_secp_instance();
+	let secp = secp_inst.lock();
+
+	let mut shared_pubkey = other_pubkey.clone();
+	shared_pubkey.mul_assign(&secp, &sec_key).unwrap();
+
+	let x_coord = shared_pubkey.serialize_vec(&secp, true);
+	SecretKey::from_slice(&secp, &x_coord[1..]).unwrap()
+}
+
 // Types to make working with json responses easier
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WalletAPIReturnError {
-	message: String,
+	pub message: String,
+	pub code: i32,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]

tests/owner_v2_sanity.rs
@@ -31,11 +31,14 @@ use grin_wallet_util::grin_keychain::ExtKeychain;
 #[macro_use]
 mod common;
 use common::RetrieveSummaryInfoResp;
-use common::{execute_command, initial_setup_wallet, instantiate_wallet, send_request, setup};
+use common::{
+	clean_output_dir, execute_command, initial_setup_wallet, instantiate_wallet, send_request,
+	setup,
+};
 
 #[test]
-fn owner_v3() -> Result<(), grin_wallet_controller::Error> {
-	let test_dir = "target/test_output/owner_v3";
+fn owner_v2_sanity() -> Result<(), grin_wallet_controller::Error> {
+	let test_dir = "target/test_output/owner_v2_sanity";
 	setup(test_dir);
 
 	setup_proxy!(test_dir, chain, wallet1, client1, mask1, wallet2, client2, _mask2);
@@ -44,6 +47,7 @@ fn owner_v3() -> Result<(), grin_wallet_controller::Error> {
 	let bh = 10u64;
 	let _ =
 		test_framework::award_blocks_to_wallet(&chain, wallet1.clone(), mask1, bh as usize, false);
+	let client1_2 = client1.clone();
 
 	// run the owner listener on wallet 1
 	let arg_vec = vec!["grin-wallet", "-p", "password", "owner_api"];
@@ -55,7 +59,7 @@ fn owner_v3() -> Result<(), grin_wallet_controller::Error> {
 	});
 
 	// run the foreign listener for wallet 2
-	let arg_vec = vec!["grin-wallet", "-p", "password", "listen"];
+	let arg_vec = vec!["grin-wallet", "-p", "password", "listen", "-l", "23415"];
 	// Set owner listener running
 	thread::spawn(move || {
 		let yml = load_yaml!("../src/bin/grin-wallet.yml");
@@ -65,12 +69,30 @@ fn owner_v3() -> Result<(), grin_wallet_controller::Error> {
 
 	thread::sleep(Duration::from_millis(200));
 
-	// Send simple retrieve_info request to owner listener
-	let req = include_str!("data/v3_reqs/retrieve_info.req.json");
-	let res = send_request(1, "http://127.0.0.1:3420/v3/owner", req)?;
+	// 1) Send simple retrieve_info request to owner listener
+	let req = include_str!("data/v2_reqs/retrieve_info.req.json");
+	let res = send_request(1, "http://127.0.0.1:3420/v2/owner", req)?;
 	assert!(res.is_ok());
 	let value: RetrieveSummaryInfoResp = res.unwrap();
 	assert_eq!(value.1.amount_currently_spendable, 420000000000);
-	println!("Response: {:?}", value);
+	println!("Response 1: {:?}", value);
+
+	// 2) Send to wallet 2 foreign listener
+	let arg_vec = vec![
+		"grin-wallet",
+		"-p",
+		"password",
+		"send",
+		"-d",
+		"http://127.0.0.1:23415",
+		"10",
+	];
+	let yml = load_yaml!("../src/bin/grin-wallet.yml");
+	let app = App::from_yaml(yml);
+	let res = execute_command(&app, test_dir, "wallet1", &client1_2, arg_vec.clone());
+	println!("Response 2: {:?}", res);
+	assert!(res.is_ok());
+
+	clean_output_dir(test_dir);
 	Ok(())
 }

keplernetwork/kepler-wallet

api/src/owner_rpc.rs
@@ -24,15 +24,15 @@ use crate::libwallet::{
 };
 use crate::util::Mutex;
 use crate::{Owner, OwnerRpcS};
-use easy_jsonrpc;
+use easy_jsonrpc_mw;
 use std::sync::Arc;
 
 /// Public definition used to generate Owner jsonrpc api.
 /// * When running `kepler-wallet owner_api` with defaults, the V2 api is available at
 /// `localhost:7420/v2/owner`
 /// * The endpoint only supports POST operations, with the json-rpc request as the body
-#[easy_jsonrpc::rpc]
-pub trait OwnerRpc {
+#[easy_jsonrpc_mw::rpc]
+pub trait OwnerRpc: Sync + Send {
 	/**
 	Networked version of [Owner::accounts](struct.Owner.html#method.accounts).
 
@@ -1148,7 +1148,7 @@ pub trait OwnerRpc {
 		}
 	}
 	# "#
-	# ,false, 5 ,true, false, false);
+	# ,false, 0 ,false, false, false);
 	```
 	*/
 	fn verify_slate_messages(&self, slate: VersionedSlate) -> Result<(), ErrorKind>;
@@ -1370,7 +1370,7 @@ pub fn run_doctest_owner(
 	lock_tx: bool,
 	finalize_tx: bool,
 ) -> Result<Option<serde_json::Value>, String> {
-	use easy_jsonrpc::Handler;
+	use easy_jsonrpc_mw::Handler;
 	use kepler_wallet_impls::test_framework::{self, LocalWalletClient, WalletProxy};
 	use kepler_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
 	use kepler_wallet_libwallet::{api_impl, WalletInst};
@@ -1404,7 +1404,7 @@ pub fn run_doctest_owner(
 	let mut wallet1 =
 		Box::new(DefaultWalletImpl::<LocalWalletClient>::new(client1.clone()).unwrap())
 			as Box<
-				WalletInst<
+				dyn WalletInst<
 					'static,
 					DefaultLCProvider<LocalWalletClient, ExtKeychain>,
 					LocalWalletClient,
@@ -1439,7 +1439,7 @@ pub fn run_doctest_owner(
 	let mut wallet2 =
 		Box::new(DefaultWalletImpl::<LocalWalletClient>::new(client2.clone()).unwrap())
 			as Box<
-				WalletInst<
+				dyn WalletInst<
 					'static,
 					DefaultLCProvider<LocalWalletClient, ExtKeychain>,
 					LocalWalletClient,
@@ -1547,13 +1547,15 @@ pub fn run_doctest_owner(
 
 	let mut api_owner = Owner::new(wallet1);
 	api_owner.doctest_mode = true;
-	if use_token {
+	let res = if use_token {
 		let owner_api = &api_owner as &dyn OwnerRpcS;
-		Ok(owner_api.handle_request(request).as_option())
+		owner_api.handle_request(request).as_option()
 	} else {
 		let owner_api = &api_owner as &dyn OwnerRpc;
-		Ok(owner_api.handle_request(request).as_option())
-	}
+		owner_api.handle_request(request).as_option()
+	};
+	let _ = fs::remove_dir_all(test_dir);
+	Ok(res)
 }
 
 #[doc(hidden)]
@@ -1563,39 +1565,46 @@ macro_rules! doctest_helper_json_rpc_owner_assert_response {
 		// create temporary wallet, run jsonrpc request on owner api of wallet, delete wallet, return
 		// json response.
 		// In order to prevent leaking tempdirs, This function should not panic.
-		use kepler_wallet_api::run_doctest_owner;
-		use serde_json;
-		use serde_json::Value;
-		use tempfile::tempdir;
 
-		let dir = tempdir().map_err(|e| format!("{:#?}", e)).unwrap();
-		let dir = dir
-			.path()
-			.to_str()
-			.ok_or("Failed to convert tmpdir path to string.".to_owned())
+		// These cause LMDB to run out of disk space on CircleCI
+		// disable for now on windows
+		// TODO: Fix properly
+		#[cfg(not(target_os = "windows"))]
+			{
+			use kepler_wallet_api::run_doctest_owner;
+			use serde_json;
+			use serde_json::Value;
+			use tempfile::tempdir;
+
+			let dir = tempdir().map_err(|e| format!("{:#?}", e)).unwrap();
+			let dir = dir
+				.path()
+				.to_str()
+				.ok_or("Failed to convert tmpdir path to string.".to_owned())
+				.unwrap();
+
+			let request_val: Value = serde_json::from_str($request).unwrap();
+			let expected_response: Value = serde_json::from_str($expected_response).unwrap();
+
+			let response = run_doctest_owner(
+				request_val,
+				dir,
+				$use_token,
+				$blocks_to_mine,
+				$perform_tx,
+				$lock_tx,
+				$finalize_tx,
+				)
+			.unwrap()
 			.unwrap();
 
-		let request_val: Value = serde_json::from_str($request).unwrap();
-		let expected_response: Value = serde_json::from_str($expected_response).unwrap();
-
-		let response = run_doctest_owner(
-			request_val,
-			dir,
-			$use_token,
-			$blocks_to_mine,
-			$perform_tx,
-			$lock_tx,
-			$finalize_tx,
-			)
-		.unwrap()
-		.unwrap();
-
-		if response != expected_response {
-			panic!(
-				"(left != right) \nleft: {}\nright: {}",
-				serde_json::to_string_pretty(&response).unwrap(),
-				serde_json::to_string_pretty(&expected_response).unwrap()
+			if response != expected_response {
+				panic!(
+					"(left != right) \nleft: {}\nright: {}",
+					serde_json::to_string_pretty(&response).unwrap(),
+					serde_json::to_string_pretty(&expected_response).unwrap()
 				);
+				}
 			}
 	};
 }

tests/common/mod.rs
@@ -14,6 +14,7 @@
 //! Common functions for wallet integration tests
 extern crate kepler_wallet;
 
+use kepler_wallet_config as config;
 use kepler_wallet_impls::test_framework::LocalWalletClient;
 use kepler_wallet_util::kepler_util as util;
 
@@ -23,12 +24,14 @@ use std::sync::Arc;
 use std::{env, fs};
 use util::{Mutex, ZeroingString};
 
+use kepler_wallet_api::{EncryptedRequest, EncryptedResponse};
 use kepler_wallet_config::{GlobalWalletConfig, WalletConfig, KEPLER_WALLET_DIR};
 use kepler_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
 use kepler_wallet_libwallet::{WalletInfo, WalletInst};
 use kepler_wallet_util::kepler_core::global::{self, ChainTypes};
 use kepler_wallet_util::kepler_keychain::ExtKeychain;
-use util::secp::key::SecretKey;
+use kepler_wallet_util::kepler_util::{from_hex, static_secp_instance};
+use util::secp::key::{PublicKey, SecretKey};
 
 use kepler_wallet::cmd::wallet_args;
 use kepler_wallet_util::kepler_api as api;
@@ -100,7 +103,7 @@ macro_rules! setup_proxy {
 	};
 }
 
-fn clean_output_dir(test_dir: &str) {
+pub fn clean_output_dir(test_dir: &str) {
 	let _ = fs::remove_dir_all(test_dir);
 }
 
@@ -192,7 +195,7 @@ pub fn instantiate_wallet(
 		Arc<
 			Mutex<
 				Box<
-					WalletInst<
+					dyn WalletInst<
 						'static,
 						DefaultLCProvider<'static, LocalWalletClient, ExtKeychain>,
 						LocalWalletClient,
@@ -208,7 +211,7 @@ pub fn instantiate_wallet(
 	wallet_config.chain_type = None;
 	let mut wallet = Box::new(DefaultWalletImpl::<LocalWalletClient>::new(node_client).unwrap())
 		as Box<
-			WalletInst<
+			dyn WalletInst<
 				DefaultLCProvider<'static, LocalWalletClient, ExtKeychain>,
 				LocalWalletClient,
 				ExtKeychain,
@@ -247,6 +250,25 @@ pub fn execute_command(
 	wallet_args::wallet_command(&args, config.clone(), client.clone(), true)
 }
 
+// as above, but without necessarily setting up the wallet
+#[allow(dead_code)]
+pub fn execute_command_no_setup(
+	app: &App,
+	test_dir: &str,
+	wallet_name: &str,
+	client: &LocalWalletClient,
+	arg_vec: Vec<&str>,
+) -> Result<String, kepler_wallet_controller::Error> {
+	let args = app.clone().get_matches_from(arg_vec);
+	let _ = get_wallet_subcommand(test_dir, wallet_name, args.clone());
+	let config = config::initial_setup_wallet(&ChainTypes::AutomatedTesting, None).unwrap();
+	let mut wallet_config = config.members.unwrap().wallet.clone();
+	wallet_config.chain_type = None;
+	wallet_config.api_secret_path = None;
+	wallet_config.node_api_secret_path = None;
+	wallet_args::wallet_command(&args, wallet_config, client.clone(), true)
+}
+
 pub fn post<IN>(url: &Url, api_secret: Option<String>, input: &IN) -> Result<String, api::Error>
 where
 	IN: Serialize,
@@ -257,6 +279,7 @@ where
 	Ok(res)
 }
 
+#[allow(dead_code)]
 pub fn send_request<OUT>(
 	id: u64,
 	dest: &str,
@@ -266,19 +289,30 @@ where
 	OUT: DeserializeOwned,
 {
 	let url = Url::parse(dest).unwrap();
-	let req: Value = serde_json::from_str(req).unwrap();
-	let res: String = post(&url, None, &req).map_err(|e| {
+	let req_val: Value = serde_json::from_str(req).unwrap();
+	let res = post(&url, None, &req_val).map_err(|e| {
 		let err_string = format!("{}", e);
 		println!("{}", err_string);
 		thread::sleep(Duration::from_millis(200));
 		e
 	})?;
+
+	let res_val: Value = serde_json::from_str(&res).unwrap();
+	// encryption error, just return the string
+	if res_val["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res_val["error"]["message"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+
 	let res = serde_json::from_str(&res).unwrap();
 	let res = easy_jsonrpc::Response::from_json_response(res).unwrap();
 	let res = res.outputs.get(&id).unwrap().clone().unwrap();
 	if res["Err"] != json!(null) {
 		Ok(Err(WalletAPIReturnError {
 			message: res["Err"].as_str().unwrap().to_owned(),
+			code: res["error"]["code"].as_i64().unwrap() as i32,
 		}))
 	} else {
 		// deserialize result into expected type
@@ -287,10 +321,88 @@ where
 	}
 }
 
+#[allow(dead_code)]
+pub fn send_request_enc<OUT>(
+	sec_req_id: u32,
+	internal_request_id: u32,
+	dest: &str,
+	req: &str,
+	shared_key: &SecretKey,
+) -> Result<Result<OUT, WalletAPIReturnError>, api::Error>
+where
+	OUT: DeserializeOwned,
+{
+	let url = Url::parse(dest).unwrap();
+	let req_val: Value = serde_json::from_str(req).unwrap();
+	let req = EncryptedRequest::from_json(sec_req_id, &req_val, &shared_key).unwrap();
+	let res = post(&url, None, &req).map_err(|e| {
+		let err_string = format!("{}", e);
+		println!("{}", err_string);
+		thread::sleep(Duration::from_millis(200));
+		e
+	})?;
+
+	let res_val: Value = serde_json::from_str(&res).unwrap();
+	// encryption error, just return the string
+	if res_val["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res_val["error"]["message"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+
+	let enc_resp: EncryptedResponse = serde_json::from_str(&res).unwrap();
+	let res = enc_resp.decrypt(shared_key).unwrap();
+	if res["error"] != json!(null) {
+		return Ok(Err(WalletAPIReturnError {
+			message: res["error"]["message"].as_str().unwrap().to_owned(),
+			code: res["error"]["code"].as_i64().unwrap() as i32,
+		}));
+	}
+	let res = easy_jsonrpc::Response::from_json_response(res).unwrap();
+	let res = res
+		.outputs
+		.get(&(internal_request_id as u64))
+		.unwrap()
+		.clone()
+		.unwrap();
+
+	if res["Err"] != json!(null) {
+		Ok(Err(WalletAPIReturnError {
+			message: res["Err"].as_str().unwrap().to_owned(),
+			code: res_val["error"]["code"].as_i64().unwrap() as i32,
+		}))
+	} else {
+		// deserialize result into expected type
+		let value: OUT = serde_json::from_value(res["Ok"].clone()).unwrap();
+		Ok(Ok(value))
+	}
+}
+
+#[allow(dead_code)]
+pub fn derive_ecdh_key(sec_key_str: &str, other_pubkey: &PublicKey) -> SecretKey {
+	let sec_key_bytes = from_hex(sec_key_str.to_owned()).unwrap();
+	let sec_key = {
+		let secp_inst = static_secp_instance();
+		let secp = secp_inst.lock();
+		SecretKey::from_slice(&secp, &sec_key_bytes).unwrap()
+	};
+
+	let secp_inst = static_secp_instance();
+	let secp = secp_inst.lock();
+
+	let mut shared_pubkey = other_pubkey.clone();
+	shared_pubkey.mul_assign(&secp, &sec_key).unwrap();
+
+	let x_coord = shared_pubkey.serialize_vec(&secp, true);
+	SecretKey::from_slice(&secp, &x_coord[1..]).unwrap()
+}
+
 // Types to make working with json responses easier
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WalletAPIReturnError {
-	message: String,
+	pub message: String,
+	pub code: i32,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]

tests/owner_v2_sanity.rs
@@ -31,11 +31,14 @@ use kepler_wallet_util::kepler_keychain::ExtKeychain;
 #[macro_use]
 mod common;
 use common::RetrieveSummaryInfoResp;
-use common::{execute_command, initial_setup_wallet, instantiate_wallet, send_request, setup};
+use common::{
+	clean_output_dir, execute_command, initial_setup_wallet, instantiate_wallet, send_request,
+	setup,
+};
 
 #[test]
-fn owner_v3() -> Result<(), kepler_wallet_controller::Error> {
-	let test_dir = "target/test_output/owner_v3";
+fn owner_v2_sanity() -> Result<(), kepler_wallet_controller::Error> {
+	let test_dir = "target/test_output/owner_v2_sanity";
 	setup(test_dir);
 
 	setup_proxy!(test_dir, chain, wallet1, client1, mask1, wallet2, client2, _mask2);
@@ -44,6 +47,7 @@ fn owner_v3() -> Result<(), kepler_wallet_controller::Error> {
 	let bh = 10u64;
 	let _ =
 		test_framework::award_blocks_to_wallet(&chain, wallet1.clone(), mask1, bh as usize, false);
+	let client1_2 = client1.clone();
 
 	// run the owner listener on wallet 1
 	let arg_vec = vec!["kepler-wallet", "-p", "password", "owner_api"];
@@ -55,7 +59,7 @@ fn owner_v3() -> Result<(), kepler_wallet_controller::Error> {
 	});
 
 	// run the foreign listener for wallet 2
-	let arg_vec = vec!["kepler-wallet", "-p", "password", "listen"];
+	let arg_vec = vec!["kepler-wallet", "-p", "password", "listen", "-l", "23415"];
 	// Set owner listener running
 	thread::spawn(move || {
 		let yml = load_yaml!("../src/bin/kepler-wallet.yml");
@@ -65,12 +69,30 @@ fn owner_v3() -> Result<(), kepler_wallet_controller::Error> {
 
 	thread::sleep(Duration::from_millis(200));
 
-	// Send simple retrieve_info request to owner listener
-	let req = include_str!("data/v3_reqs/retrieve_info.req.json");
-	let res = send_request(1, "http://127.0.0.1:3420/v3/owner", req)?;
+	// 1) Send simple retrieve_info request to owner listener
+	let req = include_str!("data/v2_reqs/retrieve_info.req.json");
+	let res = send_request(1, "http://127.0.0.1:3420/v2/owner", req)?;
 	assert!(res.is_ok());
 	let value: RetrieveSummaryInfoResp = res.unwrap();
 	assert_eq!(value.1.amount_currently_spendable, 420000000000);
-	println!("Response: {:?}", value);
+	println!("Response 1: {:?}", value);
+
+	// 2) Send to wallet 2 foreign listener
+	let arg_vec = vec![
+		"kepler-wallet",
+		"-p",
+		"password",
+		"send",
+		"-d",
+		"http://127.0.0.1:23415",
+		"10",
+	];
+	let yml = load_yaml!("../src/bin/kepler-wallet.yml");
+	let app = App::from_yaml(yml);
+	let res = execute_command(&app, test_dir, "wallet1", &client1_2, arg_vec.clone());
+	println!("Response 2: {:?}", res);
+	assert!(res.is_ok());
+
+	clean_output_dir(test_dir);
 	Ok(())
 }