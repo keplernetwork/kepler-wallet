mimblewimble/grin-wallet

Tx combinator refactor (#249)
* update tx building to use simpler kernel feature handling

* pass in tx to build::partial_transaction
and update the kernel on the tx based on features from the slate

* bump SHA for grin dependencies

libwallet/src/internal/tx.rs
@@ -365,6 +365,7 @@ where
 
 #[cfg(test)]
 mod test {
+	use crate::grin_core::core::KernelFeatures;
 	use crate::grin_core::libtx::{build, ProofBuilder};
 	use crate::grin_keychain::{ExtKeychain, ExtKeychainPath, Keychain};
 
@@ -377,12 +378,14 @@ mod test {
 		let key_id1 = ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
 
 		let tx1 = build::transaction(
+			KernelFeatures::Plain { fee: 0 },
 			vec![build::output(105, key_id1.clone())],
 			&keychain,
 			&builder,
 		)
 		.unwrap();
 		let tx2 = build::transaction(
+			KernelFeatures::Plain { fee: 0 },
 			vec![build::input(105, key_id1.clone())],
 			&keychain,
 			&builder,

keplernetwork/kepler-wallet

libwallet/src/internal/tx.rs
@@ -365,6 +365,7 @@ where
 
 #[cfg(test)]
 mod test {
+	use crate::kepler_core::core::KernelFeatures;
 	use crate::kepler_core::libtx::{build, ProofBuilder};
 	use crate::kepler_keychain::{ExtKeychain, ExtKeychainPath, Keychain};
 
@@ -377,12 +378,14 @@ mod test {
 		let key_id1 = ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
 
 		let tx1 = build::transaction(
+			KernelFeatures::Plain { fee: 0 },
 			vec![build::output(105, key_id1.clone())],
 			&keychain,
 			&builder,
 		)
 		.unwrap();
 		let tx2 = build::transaction(
+			KernelFeatures::Plain { fee: 0 },
 			vec![build::input(105, key_id1.clone())],
 			&keychain,
 			&builder,