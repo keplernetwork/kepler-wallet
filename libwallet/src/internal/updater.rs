mimblewimble/grin-wallet

Always store and update kernel excess (#231)
* always store and update kernel excess

* fix to parent key id on update as caught by testing

* save kernel excess for transaction recipients and coinbase outputs

* rustfmt

* logic cleanup, cargo.lock

* update to use node beta.3 release

* fix to doctests

* check result of calc offset before using

* rustfmt

libwallet/src/internal/updater.rs
@@ -28,6 +28,7 @@ use crate::grin_keychain::{Identifier, Keychain, SwitchCommitmentType};
 use crate::grin_util as util;
 use crate::grin_util::secp::key::SecretKey;
 use crate::grin_util::secp::pedersen;
+use crate::grin_util::static_secp_instance;
 use crate::internal::keys;
 use crate::types::{
 	NodeClient, OutputData, OutputStatus, TxLogEntry, TxLogEntryType, WalletBackend, WalletInfo,
@@ -285,6 +286,16 @@ where
 							t.amount_credited = output.value;
 							t.amount_debited = 0;
 							t.num_outputs = 1;
+							// calculate kernel excess for coinbase
+							{
+								let secp = static_secp_instance();
+								let secp = secp.lock();
+								let over_commit = secp.commit_value(output.value)?;
+								let excess =
+									secp.commit_sum(vec![commit.clone()], vec![over_commit])?;
+								t.kernel_excess = Some(excess);
+								t.kernel_lookup_min_height = Some(height);
+							}
 							t.update_confirmation_ts();
 							output.tx_log_entry = Some(log_id);
 							batch.save_tx_log_entry(t, &parent_key_id)?;

keplernetwork/kepler-wallet

libwallet/src/internal/updater.rs
@@ -29,6 +29,7 @@ use crate::kepler_keychain::{Identifier, Keychain, SwitchCommitmentType};
 use crate::kepler_util as util;
 use crate::kepler_util::secp::key::SecretKey;
 use crate::kepler_util::secp::pedersen;
+use crate::kepler_util::static_secp_instance;
 use crate::types::{
 	NodeClient, OutputData, OutputStatus, TxLogEntry, TxLogEntryType, WalletBackend, WalletInfo,
 };
@@ -285,6 +286,16 @@ where
 							t.amount_credited = output.value;
 							t.amount_debited = 0;
 							t.num_outputs = 1;
+							// calculate kernel excess for coinbase
+							{
+								let secp = static_secp_instance();
+								let secp = secp.lock();
+								let over_commit = secp.commit_value(output.value)?;
+								let excess =
+									secp.commit_sum(vec![commit.clone()], vec![over_commit])?;
+								t.kernel_excess = Some(excess);
+								t.kernel_lookup_min_height = Some(height);
+							}
 							t.update_confirmation_ts();
 							output.tx_log_entry = Some(log_id);
 							batch.save_tx_log_entry(t, &parent_key_id)?;