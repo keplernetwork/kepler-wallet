mimblewimble/grin-wallet

API Lifecycle Implementation, Pt. 1 (#211)
* allow owner api to start up without a wallet, begin to add lifecycle functions

* rustfmt

* test and typos fix

* updated with lifecycle functions

* rustfmt

* updates to allow owner api tests to be executed against local wallet proxy

* rustfmt

* fix for windows test

* add ability to pass configuration to , begin to add documentation and doctests

* add ability to pass configuration to , begin to add documentation and doctests

* doctests for lifecycle functions

* rustfmt

* ensure foreign API also has mask updated when being run along owner api, add more tests to lifecycle

* rustfmt

* documentation for lifecycle methods + init_secure_api

* rustfmt

* failing doctest

api/src/lib.rs
@@ -22,6 +22,7 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
+use grin_wallet_config as config;
 use grin_wallet_util::grin_core as core;
 use grin_wallet_util::grin_keychain as keychain;
 use grin_wallet_util::grin_util as util;

impls/src/lifecycle/default.rs
@@ -14,7 +14,9 @@
 
 //! Default wallet lifecycle provider
 
-use crate::config::{config, GlobalWalletConfig, GRIN_WALLET_DIR};
+use crate::config::{
+	config, GlobalWalletConfig, GlobalWalletConfigMembers, WalletConfig, GRIN_WALLET_DIR,
+};
 use crate::core::global;
 use crate::keychain::Keychain;
 use crate::libwallet::{Error, ErrorKind, NodeClient, WalletBackend, WalletLCProvider};
@@ -23,6 +25,8 @@ use crate::util::secp::key::SecretKey;
 use crate::util::ZeroingString;
 use crate::LMDBBackend;
 use failure::ResultExt;
+use grin_wallet_util::grin_util::LoggingConfig;
+use std::fs;
 use std::path::PathBuf;
 
 pub struct DefaultLCProvider<'a, C, K>
@@ -55,14 +59,50 @@ where
 	C: NodeClient + 'a,
 	K: Keychain + 'a,
 {
-	fn set_wallet_directory(&mut self, dir: &str) {
+	fn set_top_level_directory(&mut self, dir: &str) -> Result<(), Error> {
 		self.data_dir = dir.to_owned();
+		Ok(())
 	}
 
-	fn create_config(&self, chain_type: &global::ChainTypes, file_name: &str) -> Result<(), Error> {
+	fn get_top_level_directory(&self) -> Result<String, Error> {
+		Ok(self.data_dir.to_owned())
+	}
+
+	fn create_config(
+		&self,
+		chain_type: &global::ChainTypes,
+		file_name: &str,
+		wallet_config: Option<WalletConfig>,
+		logging_config: Option<LoggingConfig>,
+	) -> Result<(), Error> {
 		let mut default_config = GlobalWalletConfig::for_chain(chain_type);
+		let logging = match logging_config {
+			Some(l) => Some(l),
+			None => match default_config.members.as_ref() {
+				Some(m) => m.clone().logging.clone(),
+				None => None,
+			},
+		};
+		let wallet = match wallet_config {
+			Some(w) => w,
+			None => match default_config.members {
+				Some(m) => m.wallet,
+				None => WalletConfig::default(),
+			},
+		};
+		default_config = GlobalWalletConfig {
+			members: Some(GlobalWalletConfigMembers { wallet, logging }),
+			..default_config
+		};
 		let mut config_file_name = PathBuf::from(self.data_dir.clone());
 		config_file_name.push(file_name);
+
+		// create top level dir if it doesn't exist
+		let dd = PathBuf::from(self.data_dir.clone());
+		if !dd.exists() {
+			// try create
+			fs::create_dir_all(dd)?;
+		}
 
 		let mut data_dir_name = PathBuf::from(self.data_dir.clone());
 		data_dir_name.push(GRIN_WALLET_DIR);
@@ -81,7 +121,10 @@ where
 			return Ok(());
 		}
 
-		default_config.update_paths(&PathBuf::from(self.data_dir.clone()));
+		let mut abs_path = std::env::current_dir()?;
+		abs_path.push(self.data_dir.clone());
+
+		default_config.update_paths(&PathBuf::from(abs_path));
 		let res = default_config.write_to_file(config_file_name.to_str().unwrap());
 		if let Err(e) = res {
 			let msg = format!(
@@ -114,10 +157,18 @@ where
 		mnemonic: Option<ZeroingString>,
 		mnemonic_length: usize,
 		password: ZeroingString,
+		test_mode: bool,
 	) -> Result<(), Error> {
 		let mut data_dir_name = PathBuf::from(self.data_dir.clone());
 		data_dir_name.push(GRIN_WALLET_DIR);
 		let data_dir_name = data_dir_name.to_str().unwrap();
+		let exists = WalletSeed::seed_file_exists(&data_dir_name);
+		if !test_mode {
+			if let Ok(true) = exists {
+				let msg = format!("Wallet seed already exists at: {}", data_dir_name);
+				return Err(ErrorKind::WalletSeedExists(msg))?;
+			}
+		}
 		let _ = WalletSeed::init_file(&data_dir_name, mnemonic_length, mnemonic, password);
 		info!("Wallet seed file created");
 		let _wallet: LMDBBackend<'a, C, K> =

libwallet/src/types.rs
@@ -15,6 +15,7 @@
 //! Types and traits that should be provided by a wallet
 //! implementation
 
+use crate::config::WalletConfig;
 use crate::error::{Error, ErrorKind};
 use crate::grin_core::core::hash::Hash;
 use crate::grin_core::core::{Output, Transaction, TxKernel};
@@ -23,7 +24,7 @@ use crate::grin_core::{global, ser};
 use crate::grin_keychain::{Identifier, Keychain};
 use crate::grin_util::secp::key::{PublicKey, SecretKey};
 use crate::grin_util::secp::{self, pedersen, Secp256k1};
-use crate::grin_util::ZeroingString;
+use crate::grin_util::{LoggingConfig, ZeroingString};
 use crate::slate::ParticipantMessages;
 use chrono::prelude::*;
 use failure::ResultExt;
@@ -52,10 +53,20 @@ where
 {
 	/// Sets the top level system wallet directory
 	/// default is assumed to be ~/.grin/main/wallet_data (or floonet equivalent)
-	fn set_wallet_directory(&mut self, dir: &str);
+	fn set_top_level_directory(&mut self, dir: &str) -> Result<(), Error>;
+
+	/// Sets the top level system wallet directory
+	/// default is assumed to be ~/.grin/main/wallet_data (or floonet equivalent)
+	fn get_top_level_directory(&self) -> Result<String, Error>;
 
 	/// Output a grin-wallet.toml file into the current top-level system wallet directory
-	fn create_config(&self, chain_type: &global::ChainTypes, file_name: &str) -> Result<(), Error>;
+	fn create_config(
+		&self,
+		chain_type: &global::ChainTypes,
+		file_name: &str,
+		wallet_config: Option<WalletConfig>,
+		logging_config: Option<LoggingConfig>,
+	) -> Result<(), Error>;
 
 	///
 	fn create_wallet(
@@ -64,6 +75,7 @@ where
 		mnemonic: Option<ZeroingString>,
 		mnemonic_length: usize,
 		password: ZeroingString,
+		test_mode: bool,
 	) -> Result<(), Error>;
 
 	///

src/cmd/wallet_args.rs
@@ -228,7 +228,7 @@ where
 	let mut wallet = Box::new(DefaultWalletImpl::<'static, C>::new(node_client.clone()).unwrap())
 		as Box<dyn WalletInst<'static, L, C, K>>;
 	let lc = wallet.lc_provider().unwrap();
-	lc.set_wallet_directory(&config.data_file_dir);
+	let _ = lc.set_top_level_directory(&config.data_file_dir);
 	Ok(Arc::new(Mutex::new(wallet)))
 }
 
@@ -402,6 +402,9 @@ pub fn parse_owner_api_args(
 ) -> Result<(), ParseError> {
 	if let Some(port) = args.value_of("port") {
 		config.owner_api_listen_port = Some(port.parse().unwrap());
+	}
+	if args.is_present("run_foreign") {
+		config.owner_api_include_foreign = Some(true);
 	}
 	Ok(())
 }
@@ -761,14 +764,29 @@ pub fn parse_cancel_args(args: &ArgMatches) -> Result<command::CancelArgs, Parse
 	})
 }
 
-pub fn wallet_command<C>(
+pub fn wallet_command<C, F>(
 	wallet_args: &ArgMatches,
 	mut wallet_config: WalletConfig,
 	mut node_client: C,
 	test_mode: bool,
+	wallet_inst_cb: F,
 ) -> Result<String, Error>
 where
 	C: NodeClient + 'static + Clone,
+	F: FnOnce(
+		Arc<
+			Mutex<
+				Box<
+					dyn WalletInst<
+						'static,
+						DefaultLCProvider<'static, C, keychain::ExtKeychain>,
+						C,
+						keychain::ExtKeychain,
+					>,
+				>,
+			>,
+		>,
+	),
 {
 	if let Some(t) = wallet_config.chain_type.clone() {
 		core::global::set_mining_mode(t);
@@ -814,17 +832,29 @@ where
 	{
 		let mut wallet_lock = wallet.lock();
 		let lc = wallet_lock.lc_provider().unwrap();
-		lc.set_wallet_directory(&wallet_config.data_file_dir);
+		let _ = lc.set_top_level_directory(&wallet_config.data_file_dir);
 	}
 
+	// provide wallet instance back to the caller (handy for testing with
+	// local wallet proxy, etc)
+	wallet_inst_cb(wallet.clone());
+
 	// don't open wallet for certain lifecycle commands
-	let keychain_mask = match wallet_args.subcommand() {
-		("init", Some(_)) => None,
-		("recover", _) => None,
-		// Owner API can be started without a wallet present
-		// TODO: Not quite yet, next PR will deal with this
-		//("owner_api", _) => None,
-		_ => {
+	let mut open_wallet = true;
+	match wallet_args.subcommand() {
+		("init", Some(_)) => open_wallet = false,
+		("recover", _) => open_wallet = false,
+		("owner_api", _) => {
+			// If wallet exists, open it. Otherwise, that's fine too.
+			let mut wallet_lock = wallet.lock();
+			let lc = wallet_lock.lc_provider().unwrap();
+			open_wallet = lc.wallet_exists(None)?;
+		}
+		_ => {}
+	}
+
+	let keychain_mask = match open_wallet {
+		true => {
 			let mut wallet_lock = wallet.lock();
 			let lc = wallet_lock.lc_provider().unwrap();
 			let mask = lc.open_wallet(
@@ -839,6 +869,7 @@ where
 			}
 			mask
 		}
+		false => None,
 	};
 
 	let km = (&keychain_mask).as_ref();
@@ -864,7 +895,13 @@ where
 		("listen", Some(args)) => {
 			let mut c = wallet_config.clone();
 			let a = arg_parse!(parse_listen_args(&mut c, &args));
-			command::listen(wallet, keychain_mask, &c, &a, &global_wallet_args.clone())
+			command::listen(
+				wallet,
+				Arc::new(Mutex::new(keychain_mask)),
+				&c,
+				&a,
+				&global_wallet_args.clone(),
+			)
 		}
 		("owner_api", Some(args)) => {
 			let mut c = wallet_config.clone();
@@ -950,7 +987,7 @@ where
 			command::check_repair(wallet, km, a)
 		}
 		_ => {
-			let msg = format!("Unknown wallet command, use 'grin help wallet' for details");
+			let msg = format!("Unknown wallet command, use 'grin-wallet help' for details");
 			return Err(ErrorKind::ArgumentError(msg).into());
 		}
 	};

tests/common/mod.rs
@@ -27,7 +27,7 @@ use util::{Mutex, ZeroingString};
 use grin_wallet_api::{EncryptedRequest, EncryptedResponse};
 use grin_wallet_config::{GlobalWalletConfig, WalletConfig, GRIN_WALLET_DIR};
 use grin_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
-use grin_wallet_libwallet::{WalletInfo, WalletInst};
+use grin_wallet_libwallet::{NodeClient, WalletInfo, WalletInst};
 use grin_wallet_util::grin_core::global::{self, ChainTypes};
 use grin_wallet_util::grin_keychain::ExtKeychain;
 use grin_wallet_util::grin_util::{from_hex, static_secp_instance};
@@ -149,6 +149,7 @@ pub fn config_command_wallet(
 }
 
 /// Handles setup and detection of paths for wallet
+#[allow(dead_code)]
 pub fn initial_setup_wallet(dir_name: &str, wallet_name: &str) -> WalletConfig {
 	let mut current_dir;
 	current_dir = env::current_dir().unwrap_or_else(|e| {
@@ -185,6 +186,7 @@ fn get_wallet_subcommand<'a>(
 }
 //
 // Helper to create an instance of the LMDB wallet
+#[allow(dead_code)]
 pub fn instantiate_wallet(
 	mut wallet_config: WalletConfig,
 	node_client: LocalWalletClient,
@@ -226,7 +228,7 @@ pub fn instantiate_wallet(
 		top_level_wallet_dir.pop();
 		wallet_config.data_file_dir = top_level_wallet_dir.to_str().unwrap().into();
 	}
-	lc.set_wallet_directory(&wallet_config.data_file_dir);
+	let _ = lc.set_top_level_directory(&wallet_config.data_file_dir);
 	let keychain_mask = lc
 		.open_wallet(None, ZeroingString::from(passphrase), true, false)
 		.unwrap();
@@ -235,6 +237,7 @@ pub fn instantiate_wallet(
 	Ok((Arc::new(Mutex::new(wallet)), keychain_mask))
 }
 
+#[allow(dead_code)]
 pub fn execute_command(
 	app: &App,
 	test_dir: &str,
@@ -247,18 +250,36 @@ pub fn execute_command(
 	let mut config = initial_setup_wallet(test_dir, wallet_name);
 	//unset chain type so it doesn't get reset
 	config.chain_type = None;
-	wallet_args::wallet_command(&args, config.clone(), client.clone(), true)
+	wallet_args::wallet_command(&args, config.clone(), client.clone(), true, |_| {})
 }
 
 // as above, but without necessarily setting up the wallet
 #[allow(dead_code)]
-pub fn execute_command_no_setup(
+pub fn execute_command_no_setup<C, F>(
 	app: &App,
 	test_dir: &str,
 	wallet_name: &str,
-	client: &LocalWalletClient,
+	client: &C,
 	arg_vec: Vec<&str>,
-) -> Result<String, grin_wallet_controller::Error> {
+	f: F,
+) -> Result<String, grin_wallet_controller::Error>
+where
+	C: NodeClient + 'static + Clone,
+	F: FnOnce(
+		Arc<
+			Mutex<
+				Box<
+					dyn WalletInst<
+						'static,
+						DefaultLCProvider<'static, C, ExtKeychain>,
+						C,
+						ExtKeychain,
+					>,
+				>,
+			>,
+		>,
+	),
+{
 	let args = app.clone().get_matches_from(arg_vec);
 	let _ = get_wallet_subcommand(test_dir, wallet_name, args.clone());
 	let config = config::initial_setup_wallet(&ChainTypes::AutomatedTesting, None).unwrap();
@@ -266,7 +287,7 @@ pub fn execute_command_no_setup(
 	wallet_config.chain_type = None;
 	wallet_config.api_secret_path = None;
 	wallet_config.node_api_secret_path = None;
-	wallet_args::wallet_command(&args, wallet_config, client.clone(), true)
+	wallet_args::wallet_command(&args, wallet_config, client.clone(), true, f)
 }
 
 pub fn post<IN>(url: &Url, api_secret: Option<String>, input: &IN) -> Result<String, api::Error>
@@ -367,6 +388,7 @@ where
 		.clone()
 		.unwrap();
 
+	//println!("RES: {}", res);
 	if res["Err"] != json!(null) {
 		Ok(Err(WalletAPIReturnError {
 			message: res["Err"].as_str().unwrap().to_owned(),
@@ -374,8 +396,21 @@ where
 		}))
 	} else {
 		// deserialize result into expected type
-		let value: OUT = serde_json::from_value(res["Ok"].clone()).unwrap();
-		Ok(Ok(value))
+		let raw_value = res["Ok"].clone();
+		let raw_value_str = serde_json::to_string_pretty(&raw_value).unwrap();
+		//println!("Raw value: {}", raw_value_str);
+		let ok_val = serde_json::from_str(&raw_value_str);
+		match ok_val {
+			Ok(v) => {
+				let value: OUT = v;
+				Ok(Ok(value))
+			}
+			Err(e) => {
+				println!("Error deserializing: {:?}", e);
+				let value: OUT = serde_json::from_value(json!("Null")).unwrap();
+				Ok(Ok(value))
+			}
+		}
 	}
 }
 

keplernetwork/kepler-wallet

api/src/lib.rs
@@ -22,6 +22,7 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
+use kepler_wallet_config as config;
 use kepler_wallet_util::kepler_core as core;
 use kepler_wallet_util::kepler_keychain as keychain;
 use kepler_wallet_util::kepler_util as util;

impls/src/lifecycle/default.rs
@@ -14,7 +14,9 @@
 
 //! Default wallet lifecycle provider
 
-use crate::config::{config, GlobalWalletConfig, KEPLER_WALLET_DIR};
+use crate::config::{
+	config, GlobalWalletConfig, GlobalWalletConfigMembers, WalletConfig, KEPLER_WALLET_DIR,
+};
 use crate::core::global;
 use crate::keychain::Keychain;
 use crate::libwallet::{Error, ErrorKind, NodeClient, WalletBackend, WalletLCProvider};
@@ -23,6 +25,8 @@ use crate::util::secp::key::SecretKey;
 use crate::util::ZeroingString;
 use crate::LMDBBackend;
 use failure::ResultExt;
+use kepler_wallet_util::kepler_util::LoggingConfig;
+use std::fs;
 use std::path::PathBuf;
 
 pub struct DefaultLCProvider<'a, C, K>
@@ -55,14 +59,50 @@ where
 	C: NodeClient + 'a,
 	K: Keychain + 'a,
 {
-	fn set_wallet_directory(&mut self, dir: &str) {
+	fn set_top_level_directory(&mut self, dir: &str) -> Result<(), Error> {
 		self.data_dir = dir.to_owned();
+		Ok(())
 	}
 
-	fn create_config(&self, chain_type: &global::ChainTypes, file_name: &str) -> Result<(), Error> {
+	fn get_top_level_directory(&self) -> Result<String, Error> {
+		Ok(self.data_dir.to_owned())
+	}
+
+	fn create_config(
+		&self,
+		chain_type: &global::ChainTypes,
+		file_name: &str,
+		wallet_config: Option<WalletConfig>,
+		logging_config: Option<LoggingConfig>,
+	) -> Result<(), Error> {
 		let mut default_config = GlobalWalletConfig::for_chain(chain_type);
+		let logging = match logging_config {
+			Some(l) => Some(l),
+			None => match default_config.members.as_ref() {
+				Some(m) => m.clone().logging.clone(),
+				None => None,
+			},
+		};
+		let wallet = match wallet_config {
+			Some(w) => w,
+			None => match default_config.members {
+				Some(m) => m.wallet,
+				None => WalletConfig::default(),
+			},
+		};
+		default_config = GlobalWalletConfig {
+			members: Some(GlobalWalletConfigMembers { wallet, logging }),
+			..default_config
+		};
 		let mut config_file_name = PathBuf::from(self.data_dir.clone());
 		config_file_name.push(file_name);
+
+		// create top level dir if it doesn't exist
+		let dd = PathBuf::from(self.data_dir.clone());
+		if !dd.exists() {
+			// try create
+			fs::create_dir_all(dd)?;
+		}
 
 		let mut data_dir_name = PathBuf::from(self.data_dir.clone());
 		data_dir_name.push(KEPLER_WALLET_DIR);
@@ -81,7 +121,10 @@ where
 			return Ok(());
 		}
 
-		default_config.update_paths(&PathBuf::from(self.data_dir.clone()));
+		let mut abs_path = std::env::current_dir()?;
+		abs_path.push(self.data_dir.clone());
+
+		default_config.update_paths(&PathBuf::from(abs_path));
 		let res = default_config.write_to_file(config_file_name.to_str().unwrap());
 		if let Err(e) = res {
 			let msg = format!(
@@ -114,10 +157,18 @@ where
 		mnemonic: Option<ZeroingString>,
 		mnemonic_length: usize,
 		password: ZeroingString,
+		test_mode: bool,
 	) -> Result<(), Error> {
 		let mut data_dir_name = PathBuf::from(self.data_dir.clone());
 		data_dir_name.push(KEPLER_WALLET_DIR);
 		let data_dir_name = data_dir_name.to_str().unwrap();
+		let exists = WalletSeed::seed_file_exists(&data_dir_name);
+		if !test_mode {
+			if let Ok(true) = exists {
+				let msg = format!("Wallet seed already exists at: {}", data_dir_name);
+				return Err(ErrorKind::WalletSeedExists(msg))?;
+			}
+		}
 		let _ = WalletSeed::init_file(&data_dir_name, mnemonic_length, mnemonic, password);
 		info!("Wallet seed file created");
 		let _wallet: LMDBBackend<'a, C, K> =

libwallet/src/types.rs
@@ -15,6 +15,7 @@
 //! Types and traits that should be provided by a wallet
 //! implementation
 
+use crate::config::WalletConfig;
 use crate::error::{Error, ErrorKind};
 use crate::kepler_core::core::hash::Hash;
 use crate::kepler_core::core::{Output, Transaction, TxKernel};
@@ -23,7 +24,7 @@ use crate::kepler_core::{global, ser};
 use crate::kepler_keychain::{Identifier, Keychain};
 use crate::kepler_util::secp::key::{PublicKey, SecretKey};
 use crate::kepler_util::secp::{self, pedersen, Secp256k1};
-use crate::kepler_util::ZeroingString;
+use crate::kepler_util::{LoggingConfig, ZeroingString};
 use crate::slate::ParticipantMessages;
 use chrono::prelude::*;
 use failure::ResultExt;
@@ -52,10 +53,20 @@ where
 {
 	/// Sets the top level system wallet directory
 	/// default is assumed to be ~/.kepler/main/wallet_data (or floonet equivalent)
-	fn set_wallet_directory(&mut self, dir: &str);
+	fn set_top_level_directory(&mut self, dir: &str) -> Result<(), Error>;
+
+	/// Sets the top level system wallet directory
+	/// default is assumed to be ~/.kepler/main/wallet_data (or floonet equivalent)
+	fn get_top_level_directory(&self) -> Result<String, Error>;
 
 	/// Output a kepler-wallet.toml file into the current top-level system wallet directory
-	fn create_config(&self, chain_type: &global::ChainTypes, file_name: &str) -> Result<(), Error>;
+	fn create_config(
+		&self,
+		chain_type: &global::ChainTypes,
+		file_name: &str,
+		wallet_config: Option<WalletConfig>,
+		logging_config: Option<LoggingConfig>,
+	) -> Result<(), Error>;
 
 	///
 	fn create_wallet(
@@ -64,6 +75,7 @@ where
 		mnemonic: Option<ZeroingString>,
 		mnemonic_length: usize,
 		password: ZeroingString,
+		test_mode: bool,
 	) -> Result<(), Error>;
 
 	///

src/cmd/wallet_args.rs
@@ -228,7 +228,7 @@ where
 	let mut wallet = Box::new(DefaultWalletImpl::<'static, C>::new(node_client.clone()).unwrap())
 		as Box<dyn WalletInst<'static, L, C, K>>;
 	let lc = wallet.lc_provider().unwrap();
-	lc.set_wallet_directory(&config.data_file_dir);
+	let _ = lc.set_top_level_directory(&config.data_file_dir);
 	Ok(Arc::new(Mutex::new(wallet)))
 }
 
@@ -402,6 +402,9 @@ pub fn parse_owner_api_args(
 ) -> Result<(), ParseError> {
 	if let Some(port) = args.value_of("port") {
 		config.owner_api_listen_port = Some(port.parse().unwrap());
+	}
+	if args.is_present("run_foreign") {
+		config.owner_api_include_foreign = Some(true);
 	}
 	Ok(())
 }
@@ -761,14 +764,29 @@ pub fn parse_cancel_args(args: &ArgMatches) -> Result<command::CancelArgs, Parse
 	})
 }
 
-pub fn wallet_command<C>(
+pub fn wallet_command<C, F>(
 	wallet_args: &ArgMatches,
 	mut wallet_config: WalletConfig,
 	mut node_client: C,
 	test_mode: bool,
+	wallet_inst_cb: F,
 ) -> Result<String, Error>
 where
 	C: NodeClient + 'static + Clone,
+	F: FnOnce(
+		Arc<
+			Mutex<
+				Box<
+					dyn WalletInst<
+						'static,
+						DefaultLCProvider<'static, C, keychain::ExtKeychain>,
+						C,
+						keychain::ExtKeychain,
+					>,
+				>,
+			>,
+		>,
+	),
 {
 	if let Some(t) = wallet_config.chain_type.clone() {
 		core::global::set_mining_mode(t);
@@ -814,17 +832,29 @@ where
 	{
 		let mut wallet_lock = wallet.lock();
 		let lc = wallet_lock.lc_provider().unwrap();
-		lc.set_wallet_directory(&wallet_config.data_file_dir);
+		let _ = lc.set_top_level_directory(&wallet_config.data_file_dir);
 	}
 
+	// provide wallet instance back to the caller (handy for testing with
+	// local wallet proxy, etc)
+	wallet_inst_cb(wallet.clone());
+
 	// don't open wallet for certain lifecycle commands
-	let keychain_mask = match wallet_args.subcommand() {
-		("init", Some(_)) => None,
-		("recover", _) => None,
-		// Owner API can be started without a wallet present
-		// TODO: Not quite yet, next PR will deal with this
-		//("owner_api", _) => None,
-		_ => {
+	let mut open_wallet = true;
+	match wallet_args.subcommand() {
+		("init", Some(_)) => open_wallet = false,
+		("recover", _) => open_wallet = false,
+		("owner_api", _) => {
+			// If wallet exists, open it. Otherwise, that's fine too.
+			let mut wallet_lock = wallet.lock();
+			let lc = wallet_lock.lc_provider().unwrap();
+			open_wallet = lc.wallet_exists(None)?;
+		}
+		_ => {}
+	}
+
+	let keychain_mask = match open_wallet {
+		true => {
 			let mut wallet_lock = wallet.lock();
 			let lc = wallet_lock.lc_provider().unwrap();
 			let mask = lc.open_wallet(
@@ -839,6 +869,7 @@ where
 			}
 			mask
 		}
+		false => None,
 	};
 
 	let km = (&keychain_mask).as_ref();
@@ -864,7 +895,13 @@ where
 		("listen", Some(args)) => {
 			let mut c = wallet_config.clone();
 			let a = arg_parse!(parse_listen_args(&mut c, &args));
-			command::listen(wallet, keychain_mask, &c, &a, &global_wallet_args.clone())
+			command::listen(
+				wallet,
+				Arc::new(Mutex::new(keychain_mask)),
+				&c,
+				&a,
+				&global_wallet_args.clone(),
+			)
 		}
 		("owner_api", Some(args)) => {
 			let mut c = wallet_config.clone();
@@ -950,7 +987,7 @@ where
 			command::check_repair(wallet, km, a)
 		}
 		_ => {
-			let msg = format!("Unknown wallet command, use 'kepler help wallet' for details");
+			let msg = format!("Unknown wallet command, use 'kepler-wallet help' for details");
 			return Err(ErrorKind::ArgumentError(msg).into());
 		}
 	};

tests/common/mod.rs
@@ -27,7 +27,7 @@ use util::{Mutex, ZeroingString};
 use kepler_wallet_api::{EncryptedRequest, EncryptedResponse};
 use kepler_wallet_config::{GlobalWalletConfig, WalletConfig, KEPLER_WALLET_DIR};
 use kepler_wallet_impls::{DefaultLCProvider, DefaultWalletImpl};
-use kepler_wallet_libwallet::{WalletInfo, WalletInst};
+use kepler_wallet_libwallet::{NodeClient, WalletInfo, WalletInst};
 use kepler_wallet_util::kepler_core::global::{self, ChainTypes};
 use kepler_wallet_util::kepler_keychain::ExtKeychain;
 use kepler_wallet_util::kepler_util::{from_hex, static_secp_instance};
@@ -149,6 +149,7 @@ pub fn config_command_wallet(
 }
 
 /// Handles setup and detection of paths for wallet
+#[allow(dead_code)]
 pub fn initial_setup_wallet(dir_name: &str, wallet_name: &str) -> WalletConfig {
 	let mut current_dir;
 	current_dir = env::current_dir().unwrap_or_else(|e| {
@@ -185,6 +186,7 @@ fn get_wallet_subcommand<'a>(
 }
 //
 // Helper to create an instance of the LMDB wallet
+#[allow(dead_code)]
 pub fn instantiate_wallet(
 	mut wallet_config: WalletConfig,
 	node_client: LocalWalletClient,
@@ -226,7 +228,7 @@ pub fn instantiate_wallet(
 		top_level_wallet_dir.pop();
 		wallet_config.data_file_dir = top_level_wallet_dir.to_str().unwrap().into();
 	}
-	lc.set_wallet_directory(&wallet_config.data_file_dir);
+	let _ = lc.set_top_level_directory(&wallet_config.data_file_dir);
 	let keychain_mask = lc
 		.open_wallet(None, ZeroingString::from(passphrase), true, false)
 		.unwrap();
@@ -235,6 +237,7 @@ pub fn instantiate_wallet(
 	Ok((Arc::new(Mutex::new(wallet)), keychain_mask))
 }
 
+#[allow(dead_code)]
 pub fn execute_command(
 	app: &App,
 	test_dir: &str,
@@ -247,18 +250,36 @@ pub fn execute_command(
 	let mut config = initial_setup_wallet(test_dir, wallet_name);
 	//unset chain type so it doesn't get reset
 	config.chain_type = None;
-	wallet_args::wallet_command(&args, config.clone(), client.clone(), true)
+	wallet_args::wallet_command(&args, config.clone(), client.clone(), true, |_| {})
 }
 
 // as above, but without necessarily setting up the wallet
 #[allow(dead_code)]
-pub fn execute_command_no_setup(
+pub fn execute_command_no_setup<C, F>(
 	app: &App,
 	test_dir: &str,
 	wallet_name: &str,
-	client: &LocalWalletClient,
+	client: &C,
 	arg_vec: Vec<&str>,
-) -> Result<String, kepler_wallet_controller::Error> {
+	f: F,
+) -> Result<String, kepler_wallet_controller::Error>
+where
+	C: NodeClient + 'static + Clone,
+	F: FnOnce(
+		Arc<
+			Mutex<
+				Box<
+					dyn WalletInst<
+						'static,
+						DefaultLCProvider<'static, C, ExtKeychain>,
+						C,
+						ExtKeychain,
+					>,
+				>,
+			>,
+		>,
+	),
+{
 	let args = app.clone().get_matches_from(arg_vec);
 	let _ = get_wallet_subcommand(test_dir, wallet_name, args.clone());
 	let config = config::initial_setup_wallet(&ChainTypes::AutomatedTesting, None).unwrap();
@@ -266,7 +287,7 @@ pub fn execute_command_no_setup(
 	wallet_config.chain_type = None;
 	wallet_config.api_secret_path = None;
 	wallet_config.node_api_secret_path = None;
-	wallet_args::wallet_command(&args, wallet_config, client.clone(), true)
+	wallet_args::wallet_command(&args, wallet_config, client.clone(), true, f)
 }
 
 pub fn post<IN>(url: &Url, api_secret: Option<String>, input: &IN) -> Result<String, api::Error>
@@ -367,6 +388,7 @@ where
 		.clone()
 		.unwrap();
 
+	//println!("RES: {}", res);
 	if res["Err"] != json!(null) {
 		Ok(Err(WalletAPIReturnError {
 			message: res["Err"].as_str().unwrap().to_owned(),
@@ -374,8 +396,21 @@ where
 		}))
 	} else {
 		// deserialize result into expected type
-		let value: OUT = serde_json::from_value(res["Ok"].clone()).unwrap();
-		Ok(Ok(value))
+		let raw_value = res["Ok"].clone();
+		let raw_value_str = serde_json::to_string_pretty(&raw_value).unwrap();
+		//println!("Raw value: {}", raw_value_str);
+		let ok_val = serde_json::from_str(&raw_value_str);
+		match ok_val {
+			Ok(v) => {
+				let value: OUT = v;
+				Ok(Ok(value))
+			}
+			Err(e) => {
+				println!("Error deserializing: {:?}", e);
+				let value: OUT = serde_json::from_value(json!("Null")).unwrap();
+				Ok(Ok(value))
+			}
+		}
 	}
 }
 