mimblewimble/grin-wallet

Payment Proof Implementation (#259)
* refactor address generation code into libwallet, bool to flag whether to include proof, add sender address in init_send_tx

* rustfmt

* require payment proof addr as part of init_tx

* rustfmt

* store payment proof on sender transaction side

* rustfmt

* change sig to ed25519 sig

* rustfmt

* add message creation and signature

* rustfmt

* add payment proof verification function

* rustfmt

* validate proof on sender side, store proof

* rustfmt

* fix json tests

* fixes and updates to tests

* added API functions for converting and retrieving proof addresses

* rustfmt

* add payment proof to init_send_tx example

* rustfmt

* incorrect comment

libwallet/src/address.rs
@@ -1,0 +1,156 @@
+// Copyright 2019 The Grin Develope;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Functions defining wallet 'addresses', i.e. ed2559 keys based on
+//! a derivation path
+
+use crate::grin_util::secp::key::SecretKey;
+use crate::{Error, ErrorKind};
+use grin_wallet_util::grin_keychain::{ChildNumber, Identifier, Keychain, SwitchCommitmentType};
+
+use data_encoding::BASE32;
+use ed25519_dalek::PublicKey as DalekPublicKey;
+use ed25519_dalek::SecretKey as DalekSecretKey;
+use failure::ResultExt;
+use sha3::{Digest, Sha3_256};
+
+use crate::blake2::blake2b::blake2b;
+
+/// Derive a secret key given a derivation path and index
+pub fn address_from_derivation_path<K>(
+	keychain: &K,
+	parent_key_id: &Identifier,
+	index: u32,
+) -> Result<SecretKey, Error>
+where
+	K: Keychain,
+{
+	let mut key_path = parent_key_id.to_path();
+	// An output derivation for acct m/0
+	// is m/0/0/0, m/0/0/1 (for instance), m/1 is m/1/0/0, m/1/0/1
+	// Address generation path should be
+	// for m/0: m/0/1/0, m/0/1/1
+	// for m/1: m/1/1/0, m/1/1/1
+	key_path.path[1] = ChildNumber::from(1);
+	key_path.depth = key_path.depth + 1;
+	key_path.path[key_path.depth as usize - 1] = ChildNumber::from(index);
+	let key_id = Identifier::from_path(&key_path);
+	let sec_key = keychain.derive_key(0, &key_id, &SwitchCommitmentType::None)?;
+	let hashed = blake2b(32, &[], &sec_key.0[..]);
+	Ok(SecretKey::from_slice(
+		&keychain.secp(),
+		&hashed.as_bytes()[..],
+	)?)
+}
+
+/// Output ed25519 keypair given an rust_secp256k1 SecretKey
+pub fn ed25519_keypair(sec_key: &SecretKey) -> Result<(DalekSecretKey, DalekPublicKey), Error> {
+	let d_skey = match DalekSecretKey::from_bytes(&sec_key.0) {
+		Ok(k) => k,
+		Err(e) => {
+			return Err(ErrorKind::ED25519Key(format!("{}", e)).to_owned())?;
+		}
+	};
+	let d_pub_key: DalekPublicKey = (&d_skey).into();
+	Ok((d_skey, d_pub_key))
+}
+
+/// Return the ed25519 public key represented in an onion address
+pub fn pubkey_from_onion_v3(onion_address: &str) -> Result<DalekPublicKey, Error> {
+	let mut input = onion_address.to_uppercase();
+	if input.starts_with("HTTP://") || input.starts_with("HTTPS://") {
+		input = input.replace("HTTP://", "");
+		input = input.replace("HTTPS://", "");
+	}
+	if input.ends_with(".ONION") {
+		input = input.replace(".ONION", "");
+	}
+	let orig_address_raw = input.clone();
+	// for now, just check input is the right length and try and decode from base32
+	if input.len() != 56 {
+		return Err(
+			ErrorKind::AddressDecoding("Input address is wrong length".to_owned()).to_owned(),
+		)?;
+	}
+	let mut address = BASE32
+		.decode(input.as_bytes())
+		.context(ErrorKind::AddressDecoding(
+			"Input address is not base 32".to_owned(),
+		))?
+		.to_vec();
+
+	address.split_off(32);
+	let key = match DalekPublicKey::from_bytes(&address) {
+		Ok(k) => k,
+		Err(_) => {
+			return Err(ErrorKind::AddressDecoding(
+				"Provided onion V3 address is invalid (parsing key)".to_owned(),
+			)
+			.to_owned())?;
+		}
+	};
+	let test_v3 = match onion_v3_from_pubkey(&key) {
+		Ok(k) => k,
+		Err(_) => {
+			return Err(ErrorKind::AddressDecoding(
+				"Provided onion V3 address is invalid (converting from pubkey)".to_owned(),
+			)
+			.to_owned())?;
+		}
+	};
+
+	if test_v3.to_uppercase() != orig_address_raw.to_uppercase() {
+		return Err(ErrorKind::AddressDecoding(
+			"Provided onion V3 address is invalid (no match)".to_owned(),
+		)
+		.to_owned())?;
+	}
+	Ok(key)
+}
+
+/// Generate an onion address from an ed25519_dalek public key
+pub fn onion_v3_from_pubkey(pub_key: &DalekPublicKey) -> Result<String, Error> {
+	// calculate checksum
+	let mut hasher = Sha3_256::new();
+	hasher.input(b".onion checksum");
+	hasher.input(pub_key.as_bytes());
+	hasher.input([0x03u8]);
+	let checksum = hasher.result();
+
+	let mut address_bytes = pub_key.as_bytes().to_vec();
+	address_bytes.push(checksum[0]);
+	address_bytes.push(checksum[1]);
+	address_bytes.push(0x03u8);
+
+	let ret = BASE32.encode(&address_bytes);
+	Ok(ret.to_lowercase())
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn onion_v3_conversion() {
+		let onion_address = "2a6at2obto3uvkpkitqp4wxcg6u36qf534eucbskqciturczzc5suyid";
+
+		let key = pubkey_from_onion_v3(onion_address).unwrap();
+		println!("Key: {:?}", &key);
+
+		let out_address = onion_v3_from_pubkey(&key).unwrap();
+		println!("Address: {:?}", &out_address);
+
+		assert_eq!(onion_address, out_address);
+	}
+}

libwallet/src/internal/tx.rs
@@ -14,17 +14,24 @@
 
 //! Transaction building functions
 
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use std::io::Cursor;
 use uuid::Uuid;
 
 use crate::grin_core::consensus::valid_header_version;
 use crate::grin_core::core::HeaderVersion;
 use crate::grin_keychain::{Identifier, Keychain};
 use crate::grin_util::secp::key::SecretKey;
+use crate::grin_util::secp::pedersen;
 use crate::grin_util::Mutex;
 use crate::internal::{selection, updater};
 use crate::slate::Slate;
-use crate::types::{Context, NodeClient, TxLogEntryType, WalletBackend};
-use crate::{Error, ErrorKind};
+use crate::types::{Context, NodeClient, StoredProofInfo, TxLogEntryType, WalletBackend};
+use crate::{address, Error, ErrorKind};
+use ed25519_dalek::Keypair as DalekKeypair;
+use ed25519_dalek::PublicKey as DalekPublicKey;
+use ed25519_dalek::SecretKey as DalekSecretKey;
+use ed25519_dalek::Signature as DalekSignature;
 
 // static for incrementing test UUIDs
 lazy_static! {
@@ -303,6 +310,7 @@ where
 pub fn update_stored_tx<'a, T: ?Sized, C, K>(
 	wallet: &mut T,
 	keychain_mask: Option<&SecretKey>,
+	context: &Context,
 	slate: &Slate,
 	is_invoiced: bool,
 ) -> Result<(), Error>
@@ -332,6 +340,29 @@ where
 	wallet.store_tx(&format!("{}", tx.tx_slate_id.unwrap()), &slate.tx)?;
 	let parent_key = tx.parent_key_id.clone();
 	tx.kernel_excess = Some(slate.tx.body.kernels[0].excess);
+
+	if let Some(ref p) = slate.payment_proof {
+		let derivation_index = match context.payment_proof_derivation_index {
+			Some(i) => i,
+			None => 0,
+		};
+		let keychain = wallet.keychain(keychain_mask)?;
+		let parent_key_id = wallet.parent_key_id();
+		let excess = slate.calc_excess(&keychain)?;
+		let sig = create_payment_proof_signature(
+			slate.amount,
+			&excess,
+			p.sender_address,
+			address::address_from_derivation_path(&keychain, &parent_key_id, derivation_index)?,
+		)?;
+		tx.payment_proof = Some(StoredProofInfo {
+			receiver_address: p.receiver_address,
+			receiver_signature: p.receiver_signature,
+			sender_address_path: derivation_index,
+			sender_signature: Some(sig),
+		})
+	}
+
 	let mut batch = wallet.batch(keychain_mask)?;
 	batch.save_tx_log_entry(tx, &parent_key)?;
 	batch.commit()?;
@@ -363,11 +394,146 @@ where
 	Ok(())
 }
 
+pub fn payment_proof_message(
+	amount: u64,
+	kernel_commitment: &pedersen::Commitment,
+	sender_address: DalekPublicKey,
+) -> Result<Vec<u8>, Error> {
+	let mut msg = Vec::new();
+	msg.write_u64::<BigEndian>(amount)?;
+	msg.append(&mut kernel_commitment.0.to_vec());
+	msg.append(&mut sender_address.to_bytes().to_vec());
+	Ok(msg)
+}
+
+pub fn _decode_payment_proof_message(
+	msg: &Vec<u8>,
+) -> Result<(u64, pedersen::Commitment, DalekPublicKey), Error> {
+	let mut rdr = Cursor::new(msg);
+	let amount = rdr.read_u64::<BigEndian>()?;
+	let mut commit_bytes = [0u8; 33];
+	for i in 0..33 {
+		commit_bytes[i] = rdr.read_u8()?;
+	}
+	let mut sender_address_bytes = [0u8; 32];
+	for i in 0..32 {
+		sender_address_bytes[i] = rdr.read_u8()?;
+	}
+
+	Ok((
+		amount,
+		pedersen::Commitment::from_vec(commit_bytes.to_vec()),
+		DalekPublicKey::from_bytes(&sender_address_bytes).unwrap(),
+	))
+}
+
+/// create a payment proof
+pub fn create_payment_proof_signature(
+	amount: u64,
+	kernel_commitment: &pedersen::Commitment,
+	sender_address: DalekPublicKey,
+	sec_key: SecretKey,
+) -> Result<DalekSignature, Error> {
+	let msg = payment_proof_message(amount, kernel_commitment, sender_address)?;
+	let d_skey = match DalekSecretKey::from_bytes(&sec_key.0) {
+		Ok(k) => k,
+		Err(e) => {
+			return Err(ErrorKind::ED25519Key(format!("{}", e)).to_owned())?;
+		}
+	};
+	let pub_key: DalekPublicKey = (&d_skey).into();
+	let keypair = DalekKeypair {
+		public: pub_key,
+		secret: d_skey,
+	};
+	Ok(keypair.sign(&msg))
+}
+
+/// Verify all aspects of a completed payment proof
+pub fn verify_payment_proof<'a, T: ?Sized, C, K>(
+	wallet: &mut T,
+	keychain_mask: Option<&SecretKey>,
+	parent_key_id: &Identifier,
+	context: &Context,
+	slate: &Slate,
+) -> Result<(), Error>
+where
+	T: WalletBackend<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	if let Some(ref p) = slate.payment_proof {
+		let keychain = wallet.keychain(keychain_mask)?;
+		let index = match context.payment_proof_derivation_index {
+			Some(i) => i,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Payment proof derivation index required".to_owned(),
+				))?;
+			}
+		};
+		let orig_sender_sk =
+			address::address_from_derivation_path(&keychain, parent_key_id, index)?;
+		let orig_sender_address = address::ed25519_keypair(&orig_sender_sk)?.1;
+		if p.sender_address != orig_sender_address {
+			return Err(ErrorKind::PaymentProof(
+				"Sender address on slate does not match original sender address".to_owned(),
+			))?;
+		}
+		let tx_vec =
+			updater::retrieve_txs(wallet, None, Some(slate.id), Some(&parent_key_id), false)?;
+		if tx_vec.len() != 1 {
+			return Err(ErrorKind::PaymentProof(
+				"TxLogEntry with original proof info not found".to_owned(),
+			))?;
+		}
+		let orig_proof_info = match tx_vec[0].clone().payment_proof {
+			Some(o) => o,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Original proof info not stored in tx".to_owned(),
+				))?;
+			}
+		};
+		if orig_proof_info.receiver_address != p.receiver_address {
+			return Err(ErrorKind::PaymentProof(
+				"Recipient address on slate does not match original recipient address".to_owned(),
+			))?;
+		}
+		let msg = payment_proof_message(
+			slate.amount,
+			&slate.calc_excess(&keychain)?,
+			orig_sender_address,
+		)?;
+		let sig = match p.receiver_signature {
+			Some(s) => s,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Recipient did not provide requested proof signature".to_owned(),
+				))?;
+			}
+		};
+
+		if let Err(_) = p.receiver_address.verify(&msg, &sig) {
+			return Err(ErrorKind::PaymentProof(
+				"Invalid proof signature".to_owned(),
+			))?;
+		};
+	}
+	Ok(())
+}
+
 #[cfg(test)]
 mod test {
+	use super::*;
+	use rand::rngs::mock::StepRng;
+
 	use crate::grin_core::core::KernelFeatures;
 	use crate::grin_core::libtx::{build, ProofBuilder};
-	use crate::grin_keychain::{ExtKeychain, ExtKeychainPath, Keychain};
+	use crate::grin_keychain::{
+		BlindSum, BlindingFactor, ExtKeychain, ExtKeychainPath, Keychain, SwitchCommitmentType,
+	};
+	use crate::grin_util::{secp, static_secp_instance};
 
 	#[test]
 	// demonstrate that input.commitment == referenced output.commitment
@@ -394,5 +560,50 @@ mod test {
 
 		assert_eq!(tx1.outputs()[0].features, tx2.inputs()[0].features);
 		assert_eq!(tx1.outputs()[0].commitment(), tx2.inputs()[0].commitment());
+	}
+
+	#[test]
+	fn payment_proof_construction() {
+		let secp_inst = static_secp_instance();
+		let secp = secp_inst.lock();
+		let mut test_rng = StepRng::new(1234567890u64, 1);
+		let sec_key = secp::key::SecretKey::new(&secp, &mut test_rng);
+		let d_skey = DalekSecretKey::from_bytes(&sec_key.0).unwrap();
+
+		let address: DalekPublicKey = (&d_skey).into();
+
+		let kernel_excess = {
+			ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
+			let keychain = ExtKeychain::from_random_seed(true).unwrap();
+			let switch = &SwitchCommitmentType::Regular;
+			let id1 = ExtKeychain::derive_key_id(1, 1, 0, 0, 0);
+			let id2 = ExtKeychain::derive_key_id(1, 2, 0, 0, 0);
+			let skey1 = keychain.derive_key(0, &id1, switch).unwrap();
+			let skey2 = keychain.derive_key(0, &id2, switch).unwrap();
+			let blinding_factor = keychain
+				.blind_sum(
+					&BlindSum::new()
+						.sub_blinding_factor(BlindingFactor::from_secret_key(skey1))
+						.add_blinding_factor(BlindingFactor::from_secret_key(skey2)),
+				)
+				.unwrap();
+			keychain
+				.secp()
+				.commit(0, blinding_factor.secret_key(&keychain.secp()).unwrap())
+				.unwrap()
+		};
+
+		let amount = 12345678u64;
+		let msg = payment_proof_message(amount, &kernel_excess, address).unwrap();
+		println!("payment proof message is (len {}): {:?}", msg.len(), msg);
+
+		let decoded = _decode_payment_proof_message(&msg).unwrap();
+		assert_eq!(decoded.0, amount);
+		assert_eq!(decoded.1, kernel_excess);
+		assert_eq!(decoded.2, address);
+
+		let sig = create_payment_proof_signature(amount, &kernel_excess, address, sec_key).unwrap();
+
+		assert!(address.verify(&msg, &sig).is_ok());
 	}
 }

keplernetwork/kepler-wallet

libwallet/src/address.rs
@@ -1,0 +1,158 @@
+// Copyright 2019 The Grin Develope;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Functions defining wallet 'addresses', i.e. ed2559 keys based on
+//! a derivation path
+
+use crate::kepler_util::secp::key::SecretKey;
+use crate::{Error, ErrorKind};
+use kepler_wallet_util::kepler_keychain::{
+	ChildNumber, Identifier, Keychain, SwitchCommitmentType,
+};
+
+use data_encoding::BASE32;
+use ed25519_dalek::PublicKey as DalekPublicKey;
+use ed25519_dalek::SecretKey as DalekSecretKey;
+use failure::ResultExt;
+use sha3::{Digest, Sha3_256};
+
+use crate::blake2::blake2b::blake2b;
+
+/// Derive a secret key given a derivation path and index
+pub fn address_from_derivation_path<K>(
+	keychain: &K,
+	parent_key_id: &Identifier,
+	index: u32,
+) -> Result<SecretKey, Error>
+where
+	K: Keychain,
+{
+	let mut key_path = parent_key_id.to_path();
+	// An output derivation for acct m/0
+	// is m/0/0/0, m/0/0/1 (for instance), m/1 is m/1/0/0, m/1/0/1
+	// Address generation path should be
+	// for m/0: m/0/1/0, m/0/1/1
+	// for m/1: m/1/1/0, m/1/1/1
+	key_path.path[1] = ChildNumber::from(1);
+	key_path.depth = key_path.depth + 1;
+	key_path.path[key_path.depth as usize - 1] = ChildNumber::from(index);
+	let key_id = Identifier::from_path(&key_path);
+	let sec_key = keychain.derive_key(0, &key_id, &SwitchCommitmentType::None)?;
+	let hashed = blake2b(32, &[], &sec_key.0[..]);
+	Ok(SecretKey::from_slice(
+		&keychain.secp(),
+		&hashed.as_bytes()[..],
+	)?)
+}
+
+/// Output ed25519 keypair given an rust_secp256k1 SecretKey
+pub fn ed25519_keypair(sec_key: &SecretKey) -> Result<(DalekSecretKey, DalekPublicKey), Error> {
+	let d_skey = match DalekSecretKey::from_bytes(&sec_key.0) {
+		Ok(k) => k,
+		Err(e) => {
+			return Err(ErrorKind::ED25519Key(format!("{}", e)).to_owned())?;
+		}
+	};
+	let d_pub_key: DalekPublicKey = (&d_skey).into();
+	Ok((d_skey, d_pub_key))
+}
+
+/// Return the ed25519 public key represented in an onion address
+pub fn pubkey_from_onion_v3(onion_address: &str) -> Result<DalekPublicKey, Error> {
+	let mut input = onion_address.to_uppercase();
+	if input.starts_with("HTTP://") || input.starts_with("HTTPS://") {
+		input = input.replace("HTTP://", "");
+		input = input.replace("HTTPS://", "");
+	}
+	if input.ends_with(".ONION") {
+		input = input.replace(".ONION", "");
+	}
+	let orig_address_raw = input.clone();
+	// for now, just check input is the right length and try and decode from base32
+	if input.len() != 56 {
+		return Err(
+			ErrorKind::AddressDecoding("Input address is wrong length".to_owned()).to_owned(),
+		)?;
+	}
+	let mut address = BASE32
+		.decode(input.as_bytes())
+		.context(ErrorKind::AddressDecoding(
+			"Input address is not base 32".to_owned(),
+		))?
+		.to_vec();
+
+	address.split_off(32);
+	let key = match DalekPublicKey::from_bytes(&address) {
+		Ok(k) => k,
+		Err(_) => {
+			return Err(ErrorKind::AddressDecoding(
+				"Provided onion V3 address is invalid (parsing key)".to_owned(),
+			)
+			.to_owned())?;
+		}
+	};
+	let test_v3 = match onion_v3_from_pubkey(&key) {
+		Ok(k) => k,
+		Err(_) => {
+			return Err(ErrorKind::AddressDecoding(
+				"Provided onion V3 address is invalid (converting from pubkey)".to_owned(),
+			)
+			.to_owned())?;
+		}
+	};
+
+	if test_v3.to_uppercase() != orig_address_raw.to_uppercase() {
+		return Err(ErrorKind::AddressDecoding(
+			"Provided onion V3 address is invalid (no match)".to_owned(),
+		)
+		.to_owned())?;
+	}
+	Ok(key)
+}
+
+/// Generate an onion address from an ed25519_dalek public key
+pub fn onion_v3_from_pubkey(pub_key: &DalekPublicKey) -> Result<String, Error> {
+	// calculate checksum
+	let mut hasher = Sha3_256::new();
+	hasher.input(b".onion checksum");
+	hasher.input(pub_key.as_bytes());
+	hasher.input([0x03u8]);
+	let checksum = hasher.result();
+
+	let mut address_bytes = pub_key.as_bytes().to_vec();
+	address_bytes.push(checksum[0]);
+	address_bytes.push(checksum[1]);
+	address_bytes.push(0x03u8);
+
+	let ret = BASE32.encode(&address_bytes);
+	Ok(ret.to_lowercase())
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn onion_v3_conversion() {
+		let onion_address = "2a6at2obto3uvkpkitqp4wxcg6u36qf534eucbskqciturczzc5suyid";
+
+		let key = pubkey_from_onion_v3(onion_address).unwrap();
+		println!("Key: {:?}", &key);
+
+		let out_address = onion_v3_from_pubkey(&key).unwrap();
+		println!("Address: {:?}", &out_address);
+
+		assert_eq!(onion_address, out_address);
+	}
+}

libwallet/src/internal/tx.rs
@@ -14,6 +14,8 @@
 
 //! Transaction building functions
 
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use std::io::Cursor;
 use uuid::Uuid;
 
 use crate::internal::{selection, updater};
@@ -21,10 +23,15 @@ use crate::kepler_core::consensus::valid_header_version;
 use crate::kepler_core::core::HeaderVersion;
 use crate::kepler_keychain::{Identifier, Keychain};
 use crate::kepler_util::secp::key::SecretKey;
+use crate::kepler_util::secp::pedersen;
 use crate::kepler_util::Mutex;
 use crate::slate::Slate;
-use crate::types::{Context, NodeClient, TxLogEntryType, WalletBackend};
-use crate::{Error, ErrorKind};
+use crate::types::{Context, NodeClient, StoredProofInfo, TxLogEntryType, WalletBackend};
+use crate::{address, Error, ErrorKind};
+use ed25519_dalek::Keypair as DalekKeypair;
+use ed25519_dalek::PublicKey as DalekPublicKey;
+use ed25519_dalek::SecretKey as DalekSecretKey;
+use ed25519_dalek::Signature as DalekSignature;
 
 // static for incrementing test UUIDs
 lazy_static! {
@@ -303,6 +310,7 @@ where
 pub fn update_stored_tx<'a, T: ?Sized, C, K>(
 	wallet: &mut T,
 	keychain_mask: Option<&SecretKey>,
+	context: &Context,
 	slate: &Slate,
 	is_invoiced: bool,
 ) -> Result<(), Error>
@@ -332,6 +340,29 @@ where
 	wallet.store_tx(&format!("{}", tx.tx_slate_id.unwrap()), &slate.tx)?;
 	let parent_key = tx.parent_key_id.clone();
 	tx.kernel_excess = Some(slate.tx.body.kernels[0].excess);
+
+	if let Some(ref p) = slate.payment_proof {
+		let derivation_index = match context.payment_proof_derivation_index {
+			Some(i) => i,
+			None => 0,
+		};
+		let keychain = wallet.keychain(keychain_mask)?;
+		let parent_key_id = wallet.parent_key_id();
+		let excess = slate.calc_excess(&keychain)?;
+		let sig = create_payment_proof_signature(
+			slate.amount,
+			&excess,
+			p.sender_address,
+			address::address_from_derivation_path(&keychain, &parent_key_id, derivation_index)?,
+		)?;
+		tx.payment_proof = Some(StoredProofInfo {
+			receiver_address: p.receiver_address,
+			receiver_signature: p.receiver_signature,
+			sender_address_path: derivation_index,
+			sender_signature: Some(sig),
+		})
+	}
+
 	let mut batch = wallet.batch(keychain_mask)?;
 	batch.save_tx_log_entry(tx, &parent_key)?;
 	batch.commit()?;
@@ -363,11 +394,146 @@ where
 	Ok(())
 }
 
+pub fn payment_proof_message(
+	amount: u64,
+	kernel_commitment: &pedersen::Commitment,
+	sender_address: DalekPublicKey,
+) -> Result<Vec<u8>, Error> {
+	let mut msg = Vec::new();
+	msg.write_u64::<BigEndian>(amount)?;
+	msg.append(&mut kernel_commitment.0.to_vec());
+	msg.append(&mut sender_address.to_bytes().to_vec());
+	Ok(msg)
+}
+
+pub fn _decode_payment_proof_message(
+	msg: &Vec<u8>,
+) -> Result<(u64, pedersen::Commitment, DalekPublicKey), Error> {
+	let mut rdr = Cursor::new(msg);
+	let amount = rdr.read_u64::<BigEndian>()?;
+	let mut commit_bytes = [0u8; 33];
+	for i in 0..33 {
+		commit_bytes[i] = rdr.read_u8()?;
+	}
+	let mut sender_address_bytes = [0u8; 32];
+	for i in 0..32 {
+		sender_address_bytes[i] = rdr.read_u8()?;
+	}
+
+	Ok((
+		amount,
+		pedersen::Commitment::from_vec(commit_bytes.to_vec()),
+		DalekPublicKey::from_bytes(&sender_address_bytes).unwrap(),
+	))
+}
+
+/// create a payment proof
+pub fn create_payment_proof_signature(
+	amount: u64,
+	kernel_commitment: &pedersen::Commitment,
+	sender_address: DalekPublicKey,
+	sec_key: SecretKey,
+) -> Result<DalekSignature, Error> {
+	let msg = payment_proof_message(amount, kernel_commitment, sender_address)?;
+	let d_skey = match DalekSecretKey::from_bytes(&sec_key.0) {
+		Ok(k) => k,
+		Err(e) => {
+			return Err(ErrorKind::ED25519Key(format!("{}", e)).to_owned())?;
+		}
+	};
+	let pub_key: DalekPublicKey = (&d_skey).into();
+	let keypair = DalekKeypair {
+		public: pub_key,
+		secret: d_skey,
+	};
+	Ok(keypair.sign(&msg))
+}
+
+/// Verify all aspects of a completed payment proof
+pub fn verify_payment_proof<'a, T: ?Sized, C, K>(
+	wallet: &mut T,
+	keychain_mask: Option<&SecretKey>,
+	parent_key_id: &Identifier,
+	context: &Context,
+	slate: &Slate,
+) -> Result<(), Error>
+where
+	T: WalletBackend<'a, C, K>,
+	C: NodeClient + 'a,
+	K: Keychain + 'a,
+{
+	if let Some(ref p) = slate.payment_proof {
+		let keychain = wallet.keychain(keychain_mask)?;
+		let index = match context.payment_proof_derivation_index {
+			Some(i) => i,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Payment proof derivation index required".to_owned(),
+				))?;
+			}
+		};
+		let orig_sender_sk =
+			address::address_from_derivation_path(&keychain, parent_key_id, index)?;
+		let orig_sender_address = address::ed25519_keypair(&orig_sender_sk)?.1;
+		if p.sender_address != orig_sender_address {
+			return Err(ErrorKind::PaymentProof(
+				"Sender address on slate does not match original sender address".to_owned(),
+			))?;
+		}
+		let tx_vec =
+			updater::retrieve_txs(wallet, None, Some(slate.id), Some(&parent_key_id), false)?;
+		if tx_vec.len() != 1 {
+			return Err(ErrorKind::PaymentProof(
+				"TxLogEntry with original proof info not found".to_owned(),
+			))?;
+		}
+		let orig_proof_info = match tx_vec[0].clone().payment_proof {
+			Some(o) => o,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Original proof info not stored in tx".to_owned(),
+				))?;
+			}
+		};
+		if orig_proof_info.receiver_address != p.receiver_address {
+			return Err(ErrorKind::PaymentProof(
+				"Recipient address on slate does not match original recipient address".to_owned(),
+			))?;
+		}
+		let msg = payment_proof_message(
+			slate.amount,
+			&slate.calc_excess(&keychain)?,
+			orig_sender_address,
+		)?;
+		let sig = match p.receiver_signature {
+			Some(s) => s,
+			None => {
+				return Err(ErrorKind::PaymentProof(
+					"Recipient did not provide requested proof signature".to_owned(),
+				))?;
+			}
+		};
+
+		if let Err(_) = p.receiver_address.verify(&msg, &sig) {
+			return Err(ErrorKind::PaymentProof(
+				"Invalid proof signature".to_owned(),
+			))?;
+		};
+	}
+	Ok(())
+}
+
 #[cfg(test)]
 mod test {
+	use super::*;
+	use rand::rngs::mock::StepRng;
+
 	use crate::kepler_core::core::KernelFeatures;
 	use crate::kepler_core::libtx::{build, ProofBuilder};
-	use crate::kepler_keychain::{ExtKeychain, ExtKeychainPath, Keychain};
+	use crate::kepler_keychain::{
+		BlindSum, BlindingFactor, ExtKeychain, ExtKeychainPath, Keychain, SwitchCommitmentType,
+	};
+	use crate::kepler_util::{secp, static_secp_instance};
 
 	#[test]
 	// demonstrate that input.commitment == referenced output.commitment
@@ -394,5 +560,50 @@ mod test {
 
 		assert_eq!(tx1.outputs()[0].features, tx2.inputs()[0].features);
 		assert_eq!(tx1.outputs()[0].commitment(), tx2.inputs()[0].commitment());
+	}
+
+	#[test]
+	fn payment_proof_construction() {
+		let secp_inst = static_secp_instance();
+		let secp = secp_inst.lock();
+		let mut test_rng = StepRng::new(1234567890u64, 1);
+		let sec_key = secp::key::SecretKey::new(&secp, &mut test_rng);
+		let d_skey = DalekSecretKey::from_bytes(&sec_key.0).unwrap();
+
+		let address: DalekPublicKey = (&d_skey).into();
+
+		let kernel_excess = {
+			ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
+			let keychain = ExtKeychain::from_random_seed(true).unwrap();
+			let switch = &SwitchCommitmentType::Regular;
+			let id1 = ExtKeychain::derive_key_id(1, 1, 0, 0, 0);
+			let id2 = ExtKeychain::derive_key_id(1, 2, 0, 0, 0);
+			let skey1 = keychain.derive_key(0, &id1, switch).unwrap();
+			let skey2 = keychain.derive_key(0, &id2, switch).unwrap();
+			let blinding_factor = keychain
+				.blind_sum(
+					&BlindSum::new()
+						.sub_blinding_factor(BlindingFactor::from_secret_key(skey1))
+						.add_blinding_factor(BlindingFactor::from_secret_key(skey2)),
+				)
+				.unwrap();
+			keychain
+				.secp()
+				.commit(0, blinding_factor.secret_key(&keychain.secp()).unwrap())
+				.unwrap()
+		};
+
+		let amount = 12345678u64;
+		let msg = payment_proof_message(amount, &kernel_excess, address).unwrap();
+		println!("payment proof message is (len {}): {:?}", msg.len(), msg);
+
+		let decoded = _decode_payment_proof_message(&msg).unwrap();
+		assert_eq!(decoded.0, amount);
+		assert_eq!(decoded.1, kernel_excess);
+		assert_eq!(decoded.2, address);
+
+		let sig = create_payment_proof_signature(amount, &kernel_excess, address, sec_key).unwrap();
+
+		assert!(address.verify(&msg, &sig).is_ok());
 	}
 }